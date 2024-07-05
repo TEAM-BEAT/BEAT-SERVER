9849b47658e394ac8fa36172eedaed10929fda10
DongHoon Lee <125895298+hoonyworld@users.noreply.github.com>
2024-07-05 19:40:36 +0900

    [refactor] #4 - 도메인형 디렉터리 구조 변경 및 local, dev, prod 운영 환경 분리 (#5)
    * [#4] fix: 클래스 첫글자 대문자로 수정
    
    * [#4] remove: enum 삭제
    
    * [#4] feat(BaseSuccessCode): 성공 상태 관리를 위한 인터페이스 생성
    
    * [#4] feat(BaseErrorCode): 에러 상태 관리를 위한 인터페이스 생성
    
    * [#4] refactor: global 패키지로 이동
    
    * [#4] refactor: 서버 운영 환경 분리

--- src/main/java/com/beat/exception/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.beat.exception;
-
-import com.beat.exception.message.ErrorMessage;
-
-public class BadRequestException extends beatException {
-    public BadRequestException(final ErrorMessage errorMessage) {
-        super(errorMessage);
-    }
-}
--- src/main/java/com/beat/global/common/handler/GlobalExceptionHandler.java
+++ src/main/java/com/beat/global/common/handler/GlobalExceptionHandler.java
@@ -1,10 +1,10 @@
-package com.beat.common.handler;
+package com.beat.global.common.handler;
 
-import com.beat.common.dto.ErrorResponse;
-import com.beat.exception.BadRequestException;
-import com.beat.exception.ForbiddenException;
-import com.beat.exception.NotFoundException;
-import com.beat.exception.UnauthorizedException;
+import com.beat.global.common.dto.ErrorResponse;
+import com.beat.global.common.exception.BadRequestException;
+import com.beat.global.common.exception.ForbiddenException;
+import com.beat.global.common.exception.NotFoundException;
+import com.beat.global.common.exception.UnauthorizedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -18,22 +18,22 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ErrorResponse> handleBadRequestException(final BadRequestException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.of(e.getErrorMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.of(e.getBaseErrorCode()));
     }
 
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<ErrorResponse> handleUnauthorizedException(final UnauthorizedException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse.of(e.getErrorMessage()));
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse.of(e.getBaseErrorCode()));
     }
 
     @ExceptionHandler(ForbiddenException.class)
     public ResponseEntity<ErrorResponse> handleForbiddenException(final ForbiddenException e) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ErrorResponse.of(e.getErrorMessage()));
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ErrorResponse.of(e.getBaseErrorCode()));
     }
 
     @ExceptionHandler(NotFoundException.class)
     protected ResponseEntity<ErrorResponse> handleNotFoundException(final NotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.of(e.getErrorMessage()));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.of(e.getBaseErrorCode()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
