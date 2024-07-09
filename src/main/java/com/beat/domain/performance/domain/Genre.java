546f44f1c1b65b4bc6d09d53c995ffedb3e4c24c
DongHoon Lee <dhl1924@naver.com>
2024-07-10 02:23:19 +0900

    [#13] feat(Genre): 장르 열거형 생성

---
+++ src/main/java/com/beat/domain/performance/domain/Genre.java
@@ -0,0 +1,15 @@
+package com.beat.domain.performance.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Genre {
+    BAND("밴드"),
+    PLAY("연극/뮤지컬"),
+    DANCE("댄스"),
+    ETC("기타");
+
+    private final String displayName;
+}
