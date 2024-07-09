8ef616d4a2e8d9896d0df309b62d77364a233e7b
DongHoon Lee <dhl1924@naver.com>
2024-07-10 03:31:52 +0900

    [#13] refactor(ScheduleNumber): 네이밍 수정

--- src/main/java/com/beat/domain/schedule/domain/ScheduleNumber.java
+++ src/main/java/com/beat/domain/schedule/domain/ScheduleNumber.java
@@ -6,9 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ScheduleNumber {
-    FIRST("첫번째"),
-    SECOND("두번째"),
-    THIRD("세번째");
+    FIRST("1회차"),
+    SECOND("2회차"),
+    THIRD("3회차");
 
     private final String displayName;
 }
