14b4e732287b4219de4211fefc466f02d796174a
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-07-10 14:52:21 +0900

    [feat] #14 - 회원, 홍보, 등장인물, 스태프 엔티티 생성 (#16)
    * [#14] feat(Member): 멤버 엔티티 생성
    
    * [#14] feat(Promotion): 홍보 엔티티 생성
    
    * [#14] feat(Cast): 등장인물 엔티티 생성
    
    * [#14] feat(Staff): 스태프 엔티티 생성
    
    * [feat] #13 - 유저, 예매, 공연, 회차 엔티티 생성 (#15)
    
    * [#13] feat(Users): 사용자 엔티티 생성
    
    * [#13] feat(Genre): 장르 열거형 생성
    
    * [#13] feat(BankName): 은행명 열거형 생성
    
    * [#13] refactor(BaseTimeEntity): 업데이트 시간 관리 필드 활성화
    
    * [#13] feat(Performance): 공연 엔티티 생성
    
    * [#13] feat(ScheduleNumber): 회차 번호 열거형 생성
    
    * [#13] feat(Schedule): 회차 엔티티 생성
    
    * [#13] feat(Booking): 예매 엔티티 생성
    
    * [#13] refactor(ScheduleNumber): 네이밍 수정
    
    * [#14} fix: 스태프, 등장인물, 홍보 엔티티 상속관계 수정
    
    ---------
    
    Co-authored-by: DongHoon Lee <125895298+hoonyworld@users.noreply.github.com>

---
+++ src/main/java/com/beat/domain/promotion/domain/Promotion.java
@@ -0,0 +1,26 @@
+package com.beat.domain.promotion.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Promotion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long promotionId;
+
+    @Column(nullable = false)
+    private String promotionPhoto;
+
+    @Column(nullable = false)
+    private Long performanceId;
+}
---
+++ src/main/java/com/beat/domain/staff/domain/Staff.java
@@ -0,0 +1,32 @@
+package com.beat.domain.staff.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Staff {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long staffId;
+
+    @Column(nullable = false)
+    private String staffName;
+
+    @Column(nullable = false)
+    private String staffRole;
+
+    @Column(nullable = false)
+    private String staffPhoto;
+
+    @Column(nullable = false)
+    private Long performanceId;
+}
