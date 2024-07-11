b802e5172ee230fd3ed95dafef8ec3e68b31e48e
DongHoon Lee <125895298+hoonyworld@users.noreply.github.com>
2024-07-12 02:26:55 +0900

    [feat] #30 - SwaggerConfig 및 WebConfig 구현 (#31)
    * [#30] chore(build.gradle): springdoc 의존성 추가
    
    * [#30] feat(SwaggerConfig): SwaggerConfig 추가
    
    * [#30] feat(WebConfig): WebConfig 추가

---
+++ src/main/java/com/beat/global/swagger/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.beat.global.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("BEAT Project API")
+                        .description("드랍더비트")
+                        .version("1.0.0"));
+    }
+}
