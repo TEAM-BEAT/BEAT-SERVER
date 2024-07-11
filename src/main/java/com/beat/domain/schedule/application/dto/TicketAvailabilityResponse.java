5eb70a1811da850a89b72f562775583937ab670d
DongHoon Lee <dhl1924@naver.com>
2024-07-12 06:04:14 +0900

    [#32] feat(TicketAvailabilityResponse): TicketAvailabilityResponse DTO 생성

---
+++ src/main/java/com/beat/domain/schedule/application/dto/TicketAvailabilityResponse.java
@@ -0,0 +1,15 @@
+package com.beat.domain.schedule.application.dto;
+
+public record TicketAvailabilityResponse(
+        Long scheduleId,
+        String scheduleNumber,
+        int totalTicketCount,
+        int soldTicketCount,
+        int availableTicketCount,
+        int requestedTicketCount,
+        boolean isAvailable
+) {
+    public static TicketAvailabilityResponse of(Long scheduleId, String scheduleNumber, int totalTicketCount, int soldTicketCount, int availableTicketCount, int requestedTicketCount, boolean isAvailable) {
+        return new TicketAvailabilityResponse(scheduleId, scheduleNumber, totalTicketCount, soldTicketCount, availableTicketCount, requestedTicketCount, isAvailable);
+    }
+}
