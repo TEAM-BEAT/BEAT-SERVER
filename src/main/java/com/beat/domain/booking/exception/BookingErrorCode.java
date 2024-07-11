8937e751b74373a0123a96d4cb68d79150f50106
DongHoon Lee <dhl1924@naver.com>
2024-07-12 01:14:56 +0900

    [#28] feat(BookingErrorCode): 예매 에러 메시지를 관리하는 열거형 생성

---
+++ src/main/java/com/beat/domain/booking/exception/BookingErrorCode.java
@@ -0,0 +1,18 @@
+package com.beat.domain.booking.exception;
+
+import com.beat.global.common.exception.base.BaseErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum BookingErrorCode implements BaseErrorCode {
+    REQUIRED_DATA_MISSING(400, "필수 데이터가 누락되었습니다."),
+    INVALID_DATA_FORMAT(400, "잘못된 데이터 형식입니다."),
+    INVALID_REQUEST_FORMAT(400, "잘못된 요청 형식입니다."),
+    NO_BOOKING_FOUND(404, "입력하신 정보와 일치하는 예매 내역이 없습니다. 확인 후 다시 조회해주세요.")
+    ;
+
+    private final int status;
+    private final String message;
+}
