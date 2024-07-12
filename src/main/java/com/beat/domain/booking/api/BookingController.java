d3e11c56b4b625d5155fedb786dafcf4c5170b83
DongHoon Lee <dhl1924@naver.com>
2024-07-13 05:30:32 +0900

    [#34] feat(BookingController): 비회원 예매 POST API 구현

--- src/main/java/com/beat/domain/booking/api/BookingController.java
+++ src/main/java/com/beat/domain/booking/api/BookingController.java
@@ -1,8 +1,11 @@
 package com.beat.domain.booking.api;
 
-import com.beat.domain.booking.application.BookingService;
+import com.beat.domain.booking.application.BookingRetrieveService;
+import com.beat.domain.booking.application.GuestBookingService;
 import com.beat.domain.booking.application.dto.BookingRetrieveRequest;
 import com.beat.domain.booking.application.dto.BookingRetrieveResponse;
+import com.beat.domain.booking.application.dto.GuestBookingRequest;
+import com.beat.domain.booking.application.dto.GuestBookingResponse;
 import com.beat.domain.booking.exception.BookingSuccessCode;
 import com.beat.global.common.dto.SuccessResponse;
 import lombok.RequiredArgsConstructor;
@@ -20,12 +23,21 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BookingController {
 
-    private final BookingService bookingService;
+    private final BookingRetrieveService bookingRetrieveService;
+    private final GuestBookingService guestBookingService;
+
+    @PostMapping("/guest")
+    public ResponseEntity<SuccessResponse<GuestBookingResponse>> createGuestBookings(
+            @RequestBody GuestBookingRequest guestBookingRequest) {
+        GuestBookingResponse response = guestBookingService.createGuestBooking(guestBookingRequest);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(SuccessResponse.of(BookingSuccessCode.BOOKING_SUCCESS, response));
+    }
 
     @PostMapping("/guest/retrieve")
     public ResponseEntity<SuccessResponse<List<BookingRetrieveResponse>>> getGuestBookings(
             @RequestBody BookingRetrieveRequest bookingRetrieveRequest) {
-        List<BookingRetrieveResponse> response = bookingService.findGuestBookings(bookingRetrieveRequest);
+        List<BookingRetrieveResponse> response = bookingRetrieveService.findGuestBookings(bookingRetrieveRequest);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(SuccessResponse.of(BookingSuccessCode.BOOKING_RETRIEVE_SUCCESS, response));
     }
