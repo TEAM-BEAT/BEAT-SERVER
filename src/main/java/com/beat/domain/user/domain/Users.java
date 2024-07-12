19ed392d202c3dcee2ac801727719a9068e06cf8
DongHoon Lee <dhl1924@naver.com>
2024-07-13 04:30:12 +0900

    [#34] feat(Users): 빌더 및 정적 팩토리 메서드 추가

--- src/main/java/com/beat/domain/user/domain/Users.java
+++ src/main/java/com/beat/domain/user/domain/Users.java
@@ -4,16 +4,25 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
-@NoArgsConstructor(access= AccessLevel.PROTECTED)
+@Setter
 public class Users {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+    private Long id;
+
+    @Builder
+    public Users() {
+    }
+
+    public static Users create() {
+        return Users.builder()
+                .build();
+    }
 }
