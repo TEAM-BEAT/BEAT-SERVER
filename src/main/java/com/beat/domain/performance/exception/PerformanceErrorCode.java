a4006e3dd550d92b97aa968944c151402ea2627a
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-07-14 21:18:33 +0900

    [#40] feat: 공연 관련 성공, 실패 메세지 추가

--- src/main/java/com/beat/domain/performance/exception/PerformanceErrorCode.java
+++ src/main/java/com/beat/domain/performance/exception/PerformanceErrorCode.java
@@ -1,2 +1,14 @@
-package com.beat.domain.performance.exception;public enum PerformanceErrorCode {
+package com.beat.domain.performance.exception;
+
+import com.beat.global.common.exception.base.BaseErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum PerformanceErrorCode implements BaseErrorCode {
+    PERFORMANCE_NOT_FOUND(404, "해당 공연 정보를 찾을 수 없습니다.");
+
+    private final int status;
+    private final String message;
 }
