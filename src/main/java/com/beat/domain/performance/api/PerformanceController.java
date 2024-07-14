281abc1ea4f1060bf9b954fca6ec5943c4eb580f
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-07-14 21:22:11 +0900

    [#40] feat(PerformanceController): 공연 상세페이지 엔드포인트 구현

--- src/main/java/com/beat/domain/performance/api/PerformanceController.java
+++ src/main/java/com/beat/domain/performance/api/PerformanceController.java
@@ -1,2 +1,27 @@
-package com.beat.domain.performance.api;public class PerformanceController {
+package com.beat.domain.performance.api;
+
+import com.beat.domain.performance.application.dto.PerformanceDetailResponse;
+import com.beat.domain.performance.exception.PerformanceSuccessCode;
+import com.beat.domain.performance.application.PerformanceService;
+import com.beat.global.common.dto.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/performances")
+@RequiredArgsConstructor
+public class PerformanceController {
+
+    private final PerformanceService performanceService;
+
+    @GetMapping("/detail/{performanceId}")
+    public ResponseEntity<SuccessResponse<PerformanceDetailResponse>> getPerformanceDetail(
+            @PathVariable Long performanceId) {
+        PerformanceDetailResponse performanceDetail = performanceService.getPerformanceDetail(performanceId);
+        return ResponseEntity.ok(SuccessResponse.of(PerformanceSuccessCode.PERFORMANCE_RETRIEVE_SUCCESS, performanceDetail));
+    }
 }
