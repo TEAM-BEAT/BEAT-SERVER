b9f50ae35ae30bee3b441680f230edd88fe1752b
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-07-15 16:09:23 +0900

    [#49] feat: 티켓 관련 success, error code 추가

--- src/main/java/com/beat/domain/booking/exception/BookingErrorCode.java
+++ src/main/java/com/beat/domain/booking/exception/BookingErrorCode.java
@@ -10,7 +10,10 @@ public enum BookingErrorCode implements BaseErrorCode {
     REQUIRED_DATA_MISSING(400, "필수 데이터가 누락되었습니다."),
     INVALID_DATA_FORMAT(400, "잘못된 데이터 형식입니다."),
     INVALID_REQUEST_FORMAT(400, "잘못된 요청 형식입니다."),
-    NO_BOOKING_FOUND(404, "입력하신 정보와 일치하는 예매 내역이 없습니다. 확인 후 다시 조회해주세요.")
+    NO_BOOKING_FOUND(404, "입력하신 정보와 일치하는 예매 내역이 없습니다. 확인 후 다시 조회해주세요."),
+    NO_TICKETS_FOUND(404, "입력하신 정보와 일치하는 예매자 목록이 없습니다."),
+    NO_PERFORMANCE_FOUND(404, "공연을 찾을 수 없습니다."),
+    NO_SCHEDULE_FOUND(404, "회차를 찾을 수 없습니다.")
     ;
 
     private final int status;
--- src/main/java/com/beat/domain/booking/exception/BookingSuccessCode.java
+++ src/main/java/com/beat/domain/booking/exception/BookingSuccessCode.java
@@ -10,7 +10,10 @@ public enum BookingSuccessCode implements BaseSuccessCode {
     MEMBER_BOOKING_SUCCESS(201, "회원 예매가 성공적으로 완료되었습니다"),
     GUEST_BOOKING_SUCCESS(201, "비회원 예매가 성공적으로 완료되었습니다"),
     MEMBER_BOOKING_RETRIEVE_SUCCESS(200, "회원 예매 조회가 성공적으로 완료되었습니다."),
-    GUEST_BOOKING_RETRIEVE_SUCCESS(200, "비회원 예매 조회가 성공적으로 완료되었습니다.")
+    GUEST_BOOKING_RETRIEVE_SUCCESS(200, "비회원 예매 조회가 성공적으로 완료되었습니다."),
+    TICKET_RETRIEVE_SUCCESS(200, "예매자 목록 조회가 성공적으로 완료되었습니다."),
+    TICKET_UPDATE_SUCCESS(200, "예매자 입금여부 수정이 성공적으로 완료되었습니다."),
+    TICKET_DELETE_SUCCESS(200, "예매자 삭제 요청이 성공했습니다.")
     ;
 
     private final int status;
