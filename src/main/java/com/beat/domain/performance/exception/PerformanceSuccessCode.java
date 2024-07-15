b7609a897c7660e43a787f07a7e25bf65ebb3c96
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-07-16 03:00:04 +0900

    [#54] feat(PerformanceSuccessCode): success code 추가

--- src/main/java/com/beat/domain/performance/exception/PerformanceSuccessCode.java
+++ src/main/java/com/beat/domain/performance/exception/PerformanceSuccessCode.java
@@ -9,7 +9,8 @@ import lombok.RequiredArgsConstructor;
 public enum PerformanceSuccessCode implements BaseSuccessCode {
     PERFORMANCE_RETRIEVE_SUCCESS(200, "공연 상세 정보 조회가 성공적으로 완료되었습니다."),
     BOOKING_PERFORMANCE_RETRIEVE_SUCCESS(200, "예매 관련 공연 정보 조회가 성공적으로 완료되었습니다."),
-    HOME_PERFORMANCE_RETRIEVE_SUCCESS(200, "홈 화면 공연 목록 조회가 성공적으로 완료되었습니다.")
+    HOME_PERFORMANCE_RETRIEVE_SUCCESS(200, "홈 화면 공연 목록 조회가 성공적으로 완료되었습니다."),
+    MAKER_PERFORMANCE_RETRIEVE_SUCCESS(200, "회원이 등록한 공연 목록의 조회가 성공적으로 완료되었습니다.")
     ;
 
     private final int status;
