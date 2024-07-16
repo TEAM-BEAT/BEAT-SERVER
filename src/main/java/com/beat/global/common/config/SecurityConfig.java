3a92f78f83e3cd195dee20fc50e601a149c2ffa0
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-07-16 19:07:06 +0900

    [fix] #57 - security 관련 에러 대응 (#71)
    * [#57] refactor: entity 연관관계 설정 및 수정
    
    * [#57] fix(SecurityConfig): auth_whitelist update
    
    * [#57] chore(UsersRepository): 중복된 파일 및 불필요한 패키지 제거
    
    * [#57] refactor(SecurityConfig): auth_whitelist update
    
    * [#57] refactor: 엔티티 정적팩토리 메소드 추가
    
    * [#57] comment(SecurityConfig): 주석 제거
    
    * [#57] fix(JwtAuthenticationFilter): 에러 로직 수정
    
    * fix(SecurityConfig): webSecurityCustomizer 로직 추가

--- src/main/java/com/beat/global/common/config/SecurityConfig.java
+++ src/main/java/com/beat/global/common/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -30,7 +31,6 @@ public class SecurityConfig {
             "/v3/api-docs/**",
             "/swagger-ui/**",
             "/swagger-resources/**"
-
     };
 
     @Bean
@@ -54,5 +54,10 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(AUTH_WHITELIST);
     }
 }
