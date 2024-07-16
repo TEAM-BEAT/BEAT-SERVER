8312edb4de8bf24c77109bca71af47249004b815
DongHoon Lee <125895298+hoonyworld@users.noreply.github.com>
2024-07-16 13:18:26 +0900

    [feat] #63 - health check URL 화이트리스트에 등록 (#67)
    * [#63] remove(UsersRepository): 중복 클래스 삭제
    
    * [#63] feat(HealthCheckController): healthcheck 컨트롤러 생성
    
    * [#63] feat(SecurityConfig): healthcheck url white_list에 등록

--- src/main/java/com/beat/domain/user/repository/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.beat.domain.user.repository;
-
-import com.beat.domain.user.domain.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, Long> {
-}
