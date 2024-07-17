5365f48a0741f4ae1f113e9f4795f68fc376cd85
DongHoon Lee <dhl1924@naver.com>
2024-07-17 23:28:07 +0900

    [#113] refactor(PerformanceSuccessCode): 공연이 삭제되었을 때 보여주는 성공 메시지 추가

--- src/main/java/com/beat/domain/performance/exception/PerformanceSuccessCode.java
+++ src/main/java/com/beat/domain/performance/exception/PerformanceSuccessCode.java
@@ -7,8 +7,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum PerformanceSuccessCode implements BaseSuccessCode {
-    PERFORMANCE_CREATE_SUCCESS(201, "공연이 성공정으로 생성되었습니다."),
+    PERFORMANCE_CREATE_SUCCESS(201, "공연이 성공적으로 생성되었습니다."),
     PERFORMANCE_RETRIEVE_SUCCESS(200, "공연 상세 정보 조회가 성공적으로 완료되었습니다."),
+    PERFORMANCE_DELETE_SUCCESS(200, "공연이 성공적으로 삭제되었습니다."),
     BOOKING_PERFORMANCE_RETRIEVE_SUCCESS(200, "예매 관련 공연 정보 조회가 성공적으로 완료되었습니다."),
     HOME_PERFORMANCE_RETRIEVE_SUCCESS(200, "홈 화면 공연 목록 조회가 성공적으로 완료되었습니다."),
     MAKER_PERFORMANCE_RETRIEVE_SUCCESS(200, "회원이 등록한 공연 목록의 조회가 성공적으로 완료되었습니다.")
