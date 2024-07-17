afebc38ccdc7fd3983e4efccba96a28c35ca7876
DongHoon Lee <dhl1924@naver.com>
2024-07-18 06:56:08 +0900

    [#135] refactor(SwaggerConfig): SwaggerConfig 리팩토링

--- src/main/java/com/beat/global/swagger/SwaggerConfig.java
+++ src/main/java/com/beat/global/swagger/SwaggerConfig.java
@@ -1,19 +1,36 @@
 package com.beat.global.swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("BEAT Project API")
-                        .description("드랍더비트")
-                        .version("1.0.0"));
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+        return new OpenAPI().addServersItem(new Server().url("/"))
+                .components(new Components())
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+    private Info apiInfo() {
+        return new Info()
+                .title("BEAT Project API")
+                .description("간편하게 소규모 공연을 등록하고 관리할 수 있는 티켓 예매 플랫폼")
+                .version("1.0.0");
     }
 }
