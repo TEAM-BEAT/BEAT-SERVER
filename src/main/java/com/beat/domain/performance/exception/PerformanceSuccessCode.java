743a49a08f8a7b7cf4da96b3f591044ff51fd248
DongHoon Lee <dhl1924@naver.com>
2024-07-17 22:40:03 +0900

    [#110] refactor(PerformanceSuccessCode): 공연 성공 수정 메시지 추가

--- src/main/java/com/beat/domain/performance/exception/PerformanceSuccessCode.java
+++ src/main/java/com/beat/domain/performance/exception/PerformanceSuccessCode.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum PerformanceSuccessCode implements BaseSuccessCode {
-    PERFORMANCE_CREATE_SUCCESS(201, "공연이 성공정으로 생성되었습니다."),
+    PERFORMANCE_CREATE_SUCCESS(201, "공연이 성공적으로 생성되었습니다."),
+    PERFORMANCE_UPDATE_SUCCESS(200, "공연이 성공적으로 수정되었습니다."),
     PERFORMANCE_RETRIEVE_SUCCESS(200, "공연 상세 정보 조회가 성공적으로 완료되었습니다."),
     BOOKING_PERFORMANCE_RETRIEVE_SUCCESS(200, "예매 관련 공연 정보 조회가 성공적으로 완료되었습니다."),
     HOME_PERFORMANCE_RETRIEVE_SUCCESS(200, "홈 화면 공연 목록 조회가 성공적으로 완료되었습니다."),
