49374011e4617cb988cd3c854e379e07c8545e15
DongHoon Lee <dhl1924@naver.com>
2024-07-17 20:40:48 +0900

    [#50] feat(PerformanceController): 공연 생성 POST API 생성

--- src/main/java/com/beat/domain/performance/api/PerformanceController.java
+++ src/main/java/com/beat/domain/performance/api/PerformanceController.java
@@ -1,17 +1,23 @@
 package com.beat.domain.performance.api;
 
+import com.beat.domain.performance.application.PerformanceCreateService;
 import com.beat.domain.performance.application.dto.BookingPerformanceDetailResponse;
 import com.beat.domain.performance.application.dto.MakerPerformanceResponse;
 import com.beat.domain.performance.application.dto.PerformanceDetailResponse;
+import com.beat.domain.performance.application.dto.create.PerformanceRequest;
+import com.beat.domain.performance.application.dto.create.PerformanceResponse;
 import com.beat.domain.performance.exception.PerformanceSuccessCode;
 import com.beat.domain.performance.application.PerformanceService;
 import com.beat.global.auth.annotation.CurrentMember;
 import com.beat.global.common.dto.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +27,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class PerformanceController {
 
     private final PerformanceService performanceService;
+    private final PerformanceCreateService performanceCreateService;
+
+    @Operation(summary = "공연 생성 API", description = "공연을 생성하는 POST API입니다.")
+    @PostMapping
+    public ResponseEntity<SuccessResponse<PerformanceResponse>> createPerformance(
+            @CurrentMember Long userId,
+            @RequestBody PerformanceRequest performanceRequest) {
+        PerformanceResponse response = performanceCreateService.createPerformance(userId, performanceRequest);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(SuccessResponse.of(PerformanceSuccessCode.PERFORMANCE_CREATE_SUCCESS, response));
+    }
 
     @Operation(summary = "공연 상세정보 조회 API", description = "공연 상세페이지의 공연 상세정보를 조회하는 GET API입니다.")
     @GetMapping("/detail/{performanceId}")
