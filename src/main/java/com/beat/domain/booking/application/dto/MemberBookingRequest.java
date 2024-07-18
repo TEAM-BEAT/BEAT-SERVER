f30b21db8c9c388d6b992c7b460eb10f2b5d97eb
DongHoon Lee <dhl1924@naver.com>
2024-07-19 00:32:01 +0900

    [#126] refactor(MemberBookingRequest): 정적 팩토리 메서드 삭제

--- src/main/java/com/beat/domain/booking/application/dto/MemberBookingRequest.java
+++ src/main/java/com/beat/domain/booking/application/dto/MemberBookingRequest.java
@@ -1,31 +1,13 @@
 package com.beat.domain.booking.application.dto;
 
+import com.beat.domain.schedule.domain.ScheduleNumber;
+
 public record MemberBookingRequest(
         Long scheduleId,
-        String scheduleNumber,
+        ScheduleNumber scheduleNumber,
         int purchaseTicketCount,
         String bookerName,
         String bookerPhoneNumber,
         boolean isPaymentCompleted,
         int totalPaymentAmount
-) {
-    public static MemberBookingRequest of(
-            Long scheduleId,
-            String scheduleNumber,
-            int purchaseTicketCount,
-            String bookerName,
-            String bookerPhoneNumber,
-            boolean isPaymentCompleted,
-            int totalPaymentAmount
-    ) {
-        return new MemberBookingRequest(
-                scheduleId,
-                scheduleNumber,
-                purchaseTicketCount,
-                bookerName,
-                bookerPhoneNumber,
-                isPaymentCompleted,
-                totalPaymentAmount
-        );
-    }
-}
+) { }
