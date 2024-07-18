08dc281b0d62723334fce0fa35d319a40541c772
DongHoon Lee <dhl1924@naver.com>
2024-07-19 00:21:03 +0900

    [#126] chore(GuestBookingRetrieveRequest): 주석 삭제

--- src/main/java/com/beat/domain/booking/application/dto/GuestBookingRetrieveRequest.java
+++ src/main/java/com/beat/domain/booking/application/dto/GuestBookingRetrieveRequest.java
@@ -5,8 +5,4 @@ public record GuestBookingRetrieveRequest(
         String birthDate,
         String bookerPhoneNumber,
         String password
-) {
-//    public static GuestBookingRetrieveRequest of(String bookerName, String birthDate, String bookerPhoneNumber, String password) {
-//        return new GuestBookingRetrieveRequest(bookerName, birthDate, bookerPhoneNumber, password);
-//    }
-}
+) { }
