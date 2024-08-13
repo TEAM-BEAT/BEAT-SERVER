80136b351d3970efe4dac06655d396b96e6db102
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-08-14 00:39:21 +0900

    [#172] feat(BeatApplication): cron 관련 annotation 추가

--- src/main/java/com/beat/BeatApplication.java
+++ src/main/java/com/beat/BeatApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class BeatApplication {
 
