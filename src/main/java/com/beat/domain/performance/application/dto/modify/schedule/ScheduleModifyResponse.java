f36cab5075e2d7bf4b1d2e58d9d53d9e440ce33d
DongHoon Lee <dhl1924@naver.com>
2024-08-23 01:51:30 +0900

    [#185] feat(ScheduleModifyResponse): 회차 수정 응답 DTO 생성

---
+++ src/main/java/com/beat/domain/performance/application/dto/modify/schedule/ScheduleModifyResponse.java
@@ -0,0 +1,16 @@
+package com.beat.domain.performance.application.dto.modify.schedule;
+
+import com.beat.domain.schedule.domain.ScheduleNumber;
+
+import java.time.LocalDateTime;
+
+public record ScheduleModifyResponse(Long scheduleId,
+                                     LocalDateTime performanceDate,
+                                     int totalTicketCount,
+                                     int dueDate,
+                                     ScheduleNumber scheduleNumber) {
+
+    public static ScheduleModifyResponse of(Long scheduleId, LocalDateTime performanceDate, int totalTicketCount, int dueDate, ScheduleNumber scheduleNumber) {
+        return new ScheduleModifyResponse(scheduleId, performanceDate, totalTicketCount, dueDate, scheduleNumber);
+    }
+}
