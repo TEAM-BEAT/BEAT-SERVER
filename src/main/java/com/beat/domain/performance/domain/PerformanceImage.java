1f43fb443914bf4db32594c7554634a6f6494be7
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-08-22 15:49:53 +0900

    [#183] feat(PerformanceImage): 공연상세이미지 엔티티 생성

---
+++ src/main/java/com/beat/domain/performance/domain/PerformanceImage.java
@@ -0,0 +1,37 @@
+package com.beat.domain.performance.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PerformanceImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String performanceImage;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "performance_id", nullable = false)
+    private Performance performance;
+
+    @Builder
+    public PerformanceImage(String performanceImage, Performance performance) {
+        this.performanceImage = performanceImage;
+        this.performance = performance;
+    }
+
+    public static PerformanceImage create(String imageUrl, Performance performance) {
+        return PerformanceImage.builder()
+                .performanceImage(imageUrl)
+                .performance(performance)
+                .build();
+    }
+}
