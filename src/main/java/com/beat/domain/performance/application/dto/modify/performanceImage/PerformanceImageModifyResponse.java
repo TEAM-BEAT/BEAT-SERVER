45479a5ba3d934ce26ef34af2bf60c6b1a19cd97
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-08-24 14:34:42 +0900

    [#188] feat(PerformanceImageModifyResponse): PerformanceImageModifyResponse 추가

---
+++ src/main/java/com/beat/domain/performance/application/dto/modify/performanceImage/PerformanceImageModifyResponse.java
@@ -0,0 +1,11 @@
+package com.beat.domain.performance.application.dto.modify.performanceImage;
+
+public record PerformanceImageModifyResponse(
+        Long performanceImageId,
+        String performanceImage
+) {
+
+    public static PerformanceImageModifyResponse of(Long performanceImageId, String performanceImage) {
+        return new PerformanceImageModifyResponse(performanceImageId, performanceImage);
+    }
+}
