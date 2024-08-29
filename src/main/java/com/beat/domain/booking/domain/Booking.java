2e07d1a1c2b5330c6efde973afd8bcd3b1811325
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-08-29 13:26:23 +0900

    [#191] feat(Booking): bookingStatus, cancellationDate 필드 추가

--- src/main/java/com/beat/domain/booking/domain/Booking.java
+++ src/main/java/com/beat/domain/booking/domain/Booking.java
@@ -2,14 +2,7 @@ package com.beat.domain.booking.domain;
 
 import com.beat.domain.schedule.domain.Schedule;
 import com.beat.domain.user.domain.Users;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -38,10 +31,14 @@ public class Booking {
     private String bookerPhoneNumber;
 
     @Column(nullable = false)
-    private boolean isPaymentCompleted = false;
+    @Enumerated(EnumType.STRING)
+    private BookingStatus bookingStatus = BookingStatus.CHECKING_PAYMENT;
 
     @Column(nullable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(nullable = true)
+    private LocalDateTime cancellationDate;
 
     @Column(nullable = true)
     private String birthDate;
@@ -60,23 +57,23 @@ public class Booking {
     private Users users;
 
     @Builder
-    public Booking(int purchaseTicketCount, String bookerName, String bookerPhoneNumber, boolean isPaymentCompleted, String birthDate, String password, Schedule schedule, Users users) {
+    public Booking(int purchaseTicketCount, String bookerName, String bookerPhoneNumber, BookingStatus bookingStatus, String birthDate, String password, Schedule schedule, Users users) {
         this.purchaseTicketCount = purchaseTicketCount;
         this.bookerName = bookerName;
         this.bookerPhoneNumber = bookerPhoneNumber;
-        this.isPaymentCompleted = isPaymentCompleted;
+        this.bookingStatus = bookingStatus;
         this.birthDate = birthDate;
         this.password = password;
         this.schedule = schedule;
         this.users = users;
     }
 
-    public static Booking create(int purchaseTicketCount, String bookerName, String bookerPhoneNumber, boolean isPaymentCompleted, String birthDate, String password, Schedule schedule, Users users) {
+    public static Booking create(int purchaseTicketCount, String bookerName, String bookerPhoneNumber, BookingStatus bookingStatus, String birthDate, String password, Schedule schedule, Users users) {
         return Booking.builder()
                 .purchaseTicketCount(purchaseTicketCount)
                 .bookerName(bookerName)
                 .bookerPhoneNumber(bookerPhoneNumber)
-                .isPaymentCompleted(isPaymentCompleted)
+                .bookingStatus(bookingStatus)
                 .birthDate(birthDate)
                 .password(password)
                 .schedule(schedule)
@@ -84,8 +81,11 @@ public class Booking {
                 .build();
     }
 
-    public void setIsPaymentCompleted(boolean isPaymentCompleted) {
-        this.isPaymentCompleted = isPaymentCompleted;
+    public void setBookingStatus(BookingStatus bookingStatus) {
+        this.bookingStatus = bookingStatus;
+        if (bookingStatus == BookingStatus.BOOKING_CANCELLED) {
+            this.cancellationDate = LocalDateTime.now();
+        }
     }
 
 }
