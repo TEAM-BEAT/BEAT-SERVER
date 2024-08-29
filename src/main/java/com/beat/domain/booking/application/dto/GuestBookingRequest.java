d007d6acc33c5ae52722d8a49ca1bdda61d19af0
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-08-29 13:52:20 +0900

    [#191] feat(GuestBookingRequest): isPaymentCompleted를 bookingStatus로 변경

--- src/main/java/com/beat/domain/booking/application/dto/GuestBookingRequest.java
+++ src/main/java/com/beat/domain/booking/application/dto/GuestBookingRequest.java
@@ -1,5 +1,6 @@
 package com.beat.domain.booking.application.dto;
 
+import com.beat.domain.booking.domain.BookingStatus;
 import com.beat.domain.schedule.domain.ScheduleNumber;
 
 public record GuestBookingRequest(
@@ -11,9 +12,9 @@ public record GuestBookingRequest(
         String birthDate,
         String password,
         int totalPaymentAmount,
-        boolean isPaymentCompleted
+        BookingStatus bookingStatus
 ) {
-    public static GuestBookingRequest of(Long scheduleId, int purchaseTicketCount, ScheduleNumber scheduleNumber, String bookerName, String bookerPhoneNumber, String birthDate, String password, int totalPaymentAmount, boolean isPaymentCompleted) {
-        return new GuestBookingRequest(scheduleId, purchaseTicketCount, scheduleNumber, bookerName, bookerPhoneNumber, birthDate, password, totalPaymentAmount, isPaymentCompleted);
+    public static GuestBookingRequest of(Long scheduleId, int purchaseTicketCount, ScheduleNumber scheduleNumber, String bookerName, String bookerPhoneNumber, String birthDate, String password, int totalPaymentAmount, BookingStatus bookingStatus) {
+        return new GuestBookingRequest(scheduleId, purchaseTicketCount, scheduleNumber, bookerName, bookerPhoneNumber, birthDate, password, totalPaymentAmount, bookingStatus);
     }
 }
