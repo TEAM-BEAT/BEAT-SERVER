23814d809b4315b25e5236990f049e116d94e71a
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-08-29 16:18:20 +0900

    [#191] refactor(Booking): import문 변경

--- src/main/java/com/beat/domain/booking/domain/Booking.java
+++ src/main/java/com/beat/domain/booking/domain/Booking.java
@@ -2,7 +2,16 @@ package com.beat.domain.booking.domain;
 
 import com.beat.domain.schedule.domain.Schedule;
 import com.beat.domain.user.domain.Users;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
