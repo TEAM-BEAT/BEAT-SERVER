1818c1e95bed24a19d584d33566a9cb5323bf964
DongHoon Lee <dhl1924@naver.com>
2024-08-31 01:49:15 +0900

    [#190] feat(FileApi): Performance PreSigned Url 스웨거 명세서 작성

---
+++ src/main/java/com/beat/global/external/s3/controller/FileApi.java
@@ -0,0 +1,41 @@
+package com.beat.global.external.s3.controller;
+
+import com.beat.global.common.dto.ErrorResponse;
+import com.beat.global.common.dto.SuccessResponse;
+import com.beat.global.external.s3.service.dto.PresignedUrlFindAllResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Tag(name = "Image - Performance PreSigned Url", description = "이미지 업로드 할 Performance PreSigned Url 받기")
+public interface FileApi {
+
+    @Operation(summary = "공연 이미지 업로드 presigned url")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "공연 이미지를 업로드할 PreSigned url이 발행되었습니다.",
+                            content = @Content(schema = @Schema(implementation = SuccessResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "S3 PreSigned url을 받아오기에 실패했습니다.",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    )
+            }
+    )
+    ResponseEntity<SuccessResponse<PresignedUrlFindAllResponse>> generateAllPresignedUrls(
+            @RequestParam String posterImage,
+            @RequestParam(required = false) List<String> castImages,
+            @RequestParam(required = false) List<String> staffImages,
+            @RequestParam(required = false) List<String> performanceImages
+    );
+}
