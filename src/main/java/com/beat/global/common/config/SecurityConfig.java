c6089197c0b794585d838c4923b06e1964ddadf0
DongHoon Lee <dhl1924@naver.com>
2024-08-31 01:51:23 +0900

    [#190] refactor(SecurityConfig): AUTH_WHITELIST에 /error url 추가

--- src/main/java/com/beat/global/common/config/SecurityConfig.java
+++ src/main/java/com/beat/global/common/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -36,7 +35,8 @@ public class SecurityConfig {
             "/v3/api-docs/**",
             "/swagger-ui/**",
             "/swagger-resources/**",
-            "/api/files/**"
+            "/api/files/**",
+            "/error"
     };
 
     @Bean
