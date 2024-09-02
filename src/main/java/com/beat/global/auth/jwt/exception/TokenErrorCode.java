1fdf72e36cbd4aa86c972f4ea35e68a0ab611c38
DongHoon Lee <125895298+hoonyworld@users.noreply.github.com>
2024-09-02 14:49:13 +0900

    [#206] fix(TokenErrorCode): 토큰 만료 메시지 status 401로 변경 (#207)

--- src/main/java/com/beat/global/auth/jwt/exception/TokenErrorCode.java
+++ src/main/java/com/beat/global/auth/jwt/exception/TokenErrorCode.java
@@ -8,8 +8,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum TokenErrorCode implements BaseErrorCode {
 
-    AUTHENTICATION_CODE_EXPIRED(403, "토큰이 만료되었습니다"),
-    REFRESH_TOKEN_NOT_FOUND(404, "리프레쉬토큰이 없습니다"),
+    AUTHENTICATION_CODE_EXPIRED(401, "토큰이 만료되었습니다"),
+    REFRESH_TOKEN_NOT_FOUND(404, "리프레쉬 토큰이 존재하지 않습니다"),
     TOKEN_INCORRECT_ERROR(400, "잘못된 토큰입니다");
 
     private final int status;
