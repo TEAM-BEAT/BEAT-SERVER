997f2ffd205134df451339f1eb8e4e5b02328f7c
DongHoon Lee <dhl1924@naver.com>
2024-09-06 00:54:35 +0900

    [#190] refactor(LoginSuccessResponse): 로그인 성공 응답 DTO에 역할 필드 추가

--- src/main/java/com/beat/domain/member/dto/LoginSuccessResponse.java
+++ src/main/java/com/beat/domain/member/dto/LoginSuccessResponse.java
@@ -3,13 +3,15 @@ package com.beat.domain.member.dto;
 public record LoginSuccessResponse(
         String accessToken,
         String refreshToken,
-        String nickname
+        String nickname,
+        String role
 ) {
     public static LoginSuccessResponse of(
             final String accessToken,
             final String refreshToken,
-            final String nickname
+            final String nickname,
+            final String role
     ) {
-        return new LoginSuccessResponse(accessToken, refreshToken, nickname);
+        return new LoginSuccessResponse(accessToken, refreshToken, nickname, role);
     }
 }
