07a49d8ab3859cca1c81633047f52cfcdea0ee52
DongHoon Lee <dhl1924@naver.com>
2024-09-06 20:37:54 +0900

    [#190] chore(JwtAuthenticationFilter): 불필요한 import문 삭제

--- src/main/java/com/beat/global/auth/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/beat/global/auth/jwt/filter/JwtAuthenticationFilter.java
@@ -10,7 +10,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
