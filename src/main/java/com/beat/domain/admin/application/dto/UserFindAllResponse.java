49c7119e91c32dcc1d79734e43cbb76f9290594a
DongHoon Lee <dhl1924@naver.com>
2024-09-07 02:58:27 +0900

    [#190] fix(UserFindAllResponse): Users 객체가 Hibernate 프록시 객체로 반환되어 직렬화되지 못했던 부분 수정

--- src/main/java/com/beat/domain/admin/application/dto/UserFindAllResponse.java
+++ src/main/java/com/beat/domain/admin/application/dto/UserFindAllResponse.java
@@ -3,11 +3,15 @@ package com.beat.domain.admin.application.dto;
 import com.beat.domain.user.domain.Users;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record UserFindAllResponse(
-        List<Users> user
+        List<UserResponse> users
 ) {
-    public static UserFindAllResponse of(List<Users> user) {
-        return new UserFindAllResponse(user);
+    public static UserFindAllResponse of(List<Users> users) {
+        List<UserResponse> userResponses = users.stream()
+                .map(user -> new UserResponse(user.getId(), user.getRole().getRoleName()))
+                .collect(Collectors.toList());
+        return new UserFindAllResponse(userResponses);
     }
 }
