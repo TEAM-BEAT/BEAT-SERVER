98ab0c08ce2385610f090d25f96a07817b2e651c
DongHoon Lee <dhl1924@naver.com>
2024-09-06 23:56:58 +0900

    [#190] refactor(AdminController): 토큰을 줘서 역할 검증을 하도록 변경

--- src/main/java/com/beat/domain/admin/api/AdminController.java
+++ src/main/java/com/beat/domain/admin/api/AdminController.java
@@ -3,8 +3,10 @@ package com.beat.domain.admin.api;
 import com.beat.domain.admin.application.AdminService;
 import com.beat.domain.admin.exception.AdminSuccessCode;
 import com.beat.domain.admin.application.dto.UserFindAllResponse;
+import com.beat.global.auth.annotation.CurrentMember;
 import com.beat.global.common.dto.SuccessResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,13 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
+@Slf4j
 public class AdminController {
 
     private final AdminService adminService;
 
     @GetMapping("/users")
-    public ResponseEntity<SuccessResponse<UserFindAllResponse>> readAllUsers() {
-        UserFindAllResponse response = adminService.findAllUsers();
+    public ResponseEntity<SuccessResponse<UserFindAllResponse>> readAllUsers(
+            @CurrentMember Long memberId) {
+        UserFindAllResponse response = adminService.findAllUsers(memberId);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(SuccessResponse.of(AdminSuccessCode.FETCH_ALL_USERS_SUCCESS, response));
     }
