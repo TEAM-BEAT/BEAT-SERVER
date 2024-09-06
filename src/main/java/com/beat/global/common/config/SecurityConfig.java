7cc96c377607e25ec7e4c04bf0407e27e62b6813
DongHoon Lee <dhl1924@naver.com>
2024-09-06 21:52:08 +0900

    [#190] feat(SecurityConfig): ADMIN 권한 설정 추가

--- src/main/java/com/beat/global/common/config/SecurityConfig.java
+++ src/main/java/com/beat/global/common/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.beat.global.common.config;
 
+import com.beat.domain.user.domain.Role;
 import com.beat.global.auth.jwt.filter.JwtAuthenticationFilter;
 import com.beat.global.auth.security.CustomAccessDeniedHandler;
 import com.beat.global.auth.security.CustomJwtAuthenticationEntryPoint;
@@ -21,6 +22,7 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final CustomJwtAuthenticationEntryPoint customJwtAuthenticationEntryPoint;
     private final CustomAccessDeniedHandler customAccessDeniedHandler;
+
     private static final String[] AUTH_WHITELIST = {
             "/api/users/sign-up",
             "/api/users/refresh-token",
@@ -39,24 +41,25 @@ public class SecurityConfig {
             "/error"
     };
 
+    private static final String[] AUTH_ADMIN_ONLY = {
+            "/api/admin/**"
+    };
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> {
-                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-                })
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling(exception ->
-                {
-                    exception.authenticationEntryPoint(customJwtAuthenticationEntryPoint);
-                    exception.accessDeniedHandler(customAccessDeniedHandler);
-                });
+                        exception.authenticationEntryPoint(customJwtAuthenticationEntryPoint)
+                                .accessDeniedHandler(customAccessDeniedHandler));
 
-        http.authorizeHttpRequests(auth -> {
-                    auth.requestMatchers(AUTH_WHITELIST).permitAll();
-                    auth.anyRequest().authenticated();
-                })
+        http.authorizeHttpRequests(auth ->
+                        auth.requestMatchers(AUTH_WHITELIST).permitAll()
+                                .requestMatchers(AUTH_ADMIN_ONLY).hasAuthority(Role.ADMIN.getRoleName())
+                                .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
