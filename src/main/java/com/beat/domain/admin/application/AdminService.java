3701cd10b9b27d88108da5b7fd007170dc2389d3
DongHoon Lee <dhl1924@naver.com>
2024-09-07 03:03:18 +0900

    [#190] refactor(CustomJwtAuthenticationEntryPoint): 토큰에서 추출한 memberId가 DB에 존재하는지 확인하는 로직 추가

--- src/main/java/com/beat/domain/admin/application/AdminService.java
+++ src/main/java/com/beat/domain/admin/application/AdminService.java
@@ -1,16 +1,25 @@
 package com.beat.domain.admin.application;
 
 import com.beat.domain.admin.application.dto.UserFindAllResponse;
+import com.beat.domain.member.dao.MemberRepository;
+import com.beat.domain.member.exception.MemberErrorCode;
 import com.beat.domain.user.dao.UserRepository;
+import com.beat.global.common.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AdminService {
     private final UserRepository userRepository;
+    private final MemberRepository memberRepository;
 
-    public UserFindAllResponse findAllUsers() {
+    public UserFindAllResponse findAllUsers(Long memberId) {
+
+        memberRepository.findById(memberId)
+                .orElseThrow(() -> new NotFoundException(MemberErrorCode.MEMBER_NOT_FOUND));
+
         return UserFindAllResponse.of(userRepository.findAll());
     }
 }
