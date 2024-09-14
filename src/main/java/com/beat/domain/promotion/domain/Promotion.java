ca90323d32ef031f9f3ec6155c81757aee49c24c
DongHoon Lee <dhl1924@naver.com>
2024-09-14 22:10:23 +0900

    [#209] refactor(Promotion): 캐러셀의 순서를 나타내는 필드 추가

--- src/main/java/com/beat/domain/promotion/domain/Promotion.java
+++ src/main/java/com/beat/domain/promotion/domain/Promotion.java
@@ -1,7 +1,16 @@
 package com.beat.domain.promotion.domain;
 
 import com.beat.domain.performance.domain.Performance;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -29,20 +38,26 @@ public class Promotion {
     @Column(nullable = false)
     private boolean isExternal;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CarouselNumber carouselNumber;
+
     @Builder
-    public Promotion(String promotionPhoto, Performance performance, String redirectUrl, boolean isExternal) {
+    public Promotion(String promotionPhoto, Performance performance, String redirectUrl, boolean isExternal, CarouselNumber carouselNumber) {
         this.promotionPhoto = promotionPhoto;
         this.performance = performance;
         this.redirectUrl = redirectUrl;
         this.isExternal = isExternal;
+        this.carouselNumber = carouselNumber;
     }
 
-    public static Promotion create(String promotionPhoto, Performance performance, String redirectUrl, boolean isExternal) {
+    public static Promotion create(String promotionPhoto, Performance performance, String redirectUrl, boolean isExternal, CarouselNumber carouselNumber) {
         return Promotion.builder()
                 .promotionPhoto(promotionPhoto)
                 .performance(performance)
                 .redirectUrl(redirectUrl)
                 .isExternal(isExternal)
+                .carouselNumber(carouselNumber)
                 .build();
     }
 }
