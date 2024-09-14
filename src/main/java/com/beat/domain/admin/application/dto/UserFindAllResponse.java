4c5884cfc9431de6bd4df92739ffda198a2155df
DongHoon Lee <dhl1924@naver.com>
2024-09-15 02:00:55 +0900

    [#209] refactor(UserFindAllResponse): User 조회 응답 DTO를 Nested Record로 리팩토링

--- src/main/java/com/beat/domain/admin/application/dto/UserFindAllResponse.java
+++ src/main/java/com/beat/domain/admin/application/dto/UserFindAllResponse.java
@@ -3,15 +3,26 @@ package com.beat.domain.admin.application.dto;
 import com.beat.domain.user.domain.Users;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public record UserFindAllResponse(
         List<UserFindResponse> users
 ) {
-    public static UserFindAllResponse of(List<Users> users) {
+    public static UserFindAllResponse from(List<Users> users) {
         List<UserFindResponse> userFindResponses = users.stream()
-                .map(user -> new UserFindResponse(user.getId(), user.getRole().getRoleName()))
-                .collect(Collectors.toList());
+                .map(UserFindResponse::from)
+                .toList();
         return new UserFindAllResponse(userFindResponses);
+    }
+
+    public record UserFindResponse(
+            Long id,
+            String role
+    ) {
+        public static UserFindResponse from(Users user) {
+            return new UserFindResponse(
+                    user.getId(),
+                    user.getRole().getRoleName()
+            );
+        }
     }
 }
