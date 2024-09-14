a9910d5d509c75c658ab6d7a4415bbf9bca735cc
DongHoon Lee <dhl1924@naver.com>
2024-09-15 01:50:23 +0900

    [#209] refactor(AdminSuccessCode): 관리자 권한으로 캐러셀에 등록된 모든 공연 조회 성공 메시지 추가

--- src/main/java/com/beat/domain/admin/exception/AdminSuccessCode.java
+++ src/main/java/com/beat/domain/admin/exception/AdminSuccessCode.java
@@ -9,7 +9,8 @@ import lombok.RequiredArgsConstructor;
 public enum AdminSuccessCode implements BaseSuccessCode {
     FETCH_ALL_USERS_SUCCESS(200, "관리자 권한으로 모든 유저 조회에 성공하였습니다."),
     CAROUSEL_PRESIGNED_URL_ISSUED(200, "캐러셀 Presigned URL 발급 성공"),
-    BANNER_PRESIGNED_URL_ISSUED(200, "배너 Presigned URL 발급 성공")
+    BANNER_PRESIGNED_URL_ISSUED(200, "배너 Presigned URL 발급 성공"),
+    FETCH_ALL_CAROUSEL_PROMOTIONS_SUCCESS(200, "관리자 권한으로 현재 캐러셀에 등록된 모든 공연 조회에 성공하였습니다.")
     ;
 
     private final int status;
