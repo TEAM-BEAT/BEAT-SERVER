3df52adc59b7feb742a9d97c97ad7fcfee62fbba
DongHoon Lee <dhl1924@naver.com>
2024-09-15 01:58:39 +0900

    [#209] feat(PromotionService): Promotion 전체 조회 기능 구현

--- src/main/java/com/beat/domain/promotion/application/PromotionService.java
+++ src/main/java/com/beat/domain/promotion/application/PromotionService.java
@@ -1,13 +1,9 @@
 package com.beat.domain.promotion.application;
 
-import com.beat.domain.performance.domain.Performance;
 import com.beat.domain.promotion.dao.PromotionRepository;
 import com.beat.domain.promotion.domain.Promotion;
-import com.beat.domain.schedule.application.ScheduleService;
-import com.beat.domain.schedule.dao.ScheduleRepository;
-import com.beat.domain.schedule.domain.Schedule;
+import com.beat.domain.promotion.port.in.PromotionUseCase;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,30 +11,13 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class PromotionService {
+public class PromotionService implements PromotionUseCase {
 
     private final PromotionRepository promotionRepository;
-    private final ScheduleRepository scheduleRepository;
-    private final ScheduleService scheduleService;
 
-    @Scheduled(cron = "1 0 0 * * ?")
-    @Transactional
-    public void checkAndDeleteInvalidPromotions() {
-        List<Promotion> promotions = promotionRepository.findAll();
-
-        for (Promotion promotion : promotions) {
-            Performance performance = promotion.getPerformance();
-
-            if (performance == null) {
-                return;
-            }
-
-            List<Schedule> schedules = scheduleRepository.findByPerformanceId(performance.getId());
-            int minDueDate = scheduleService.getMinDueDate(schedules);
-
-            if (minDueDate < 0) {
-                promotionRepository.delete(promotion);
-            }
-        }
+    @Override
+    @Transactional(readOnly = true)
+    public List<Promotion> findAllPromotions() {
+        return promotionRepository.findAll();
     }
 }
