9c18adf8ff7ea500ba4621a3767cd7bde562564a
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-09-22 08:26:16 +0900

    [#198] feat(ScheduleRepository): isBooking true인 공연 조회 로직 추가

--- src/main/java/com/beat/domain/schedule/dao/ScheduleRepository.java
+++ src/main/java/com/beat/domain/schedule/dao/ScheduleRepository.java
@@ -24,4 +24,8 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     List<Long> findIdsByPerformanceId(@Param("performanceId") Long performanceId);
 
     int countByPerformanceId(Long performanceId);
+
+    // 기존의 PENDING 상태인 스케줄들을 조회하는 메소드
+    @Query("SELECT s FROM Schedule s WHERE s.isBooking = true")
+    List<Schedule> findPendingSchedules();
 }
