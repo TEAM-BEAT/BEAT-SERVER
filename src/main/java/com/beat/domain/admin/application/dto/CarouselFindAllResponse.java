5b5f63e8d94f51c7b334112a4cb5271dc35ac598
DongHoon Lee <dhl1924@naver.com>
2024-09-22 04:55:22 +0900

    [#209] chore: 포맷터 작동

--- src/main/java/com/beat/domain/admin/application/dto/CarouselFindAllResponse.java
+++ src/main/java/com/beat/domain/admin/application/dto/CarouselFindAllResponse.java
@@ -8,34 +8,34 @@ import java.util.List;
 import java.util.Optional;
 
 public record CarouselFindAllResponse(
-        List<CarouselFindResponse> carousels
+	List<CarouselFindResponse> carousels
 ) {
-    public static CarouselFindAllResponse from(List<Promotion> promotions) {
-        List<CarouselFindResponse> responses = promotions.stream()
-                .map(CarouselFindResponse::from)
-                .toList();
-        return new CarouselFindAllResponse(responses);
-    }
+	public static CarouselFindAllResponse from(List<Promotion> promotions) {
+		List<CarouselFindResponse> responses = promotions.stream()
+			.map(CarouselFindResponse::from)
+			.toList();
+		return new CarouselFindAllResponse(responses);
+	}
 
-    public record CarouselFindResponse(
-            Long promotionId,
-            CarouselNumber carouselNumber,
-            String newImageUrl,
-            boolean isExternal,
-            String redirectUrl,
-            Long performanceId
-    ) {
-        public static CarouselFindResponse from(Promotion promotion) {
-            return new CarouselFindResponse(
-                    promotion.getId(),
-                    promotion.getCarouselNumber(),
-                    promotion.getPromotionPhoto(),
-                    promotion.isExternal(),
-                    promotion.getRedirectUrl(),
-                    Optional.ofNullable(promotion.getPerformance())
-                            .map(Performance::getId)
-                            .orElse(null)
-            );
-        }
-    }
+	public record CarouselFindResponse(
+		Long promotionId,
+		CarouselNumber carouselNumber,
+		String newImageUrl,
+		boolean isExternal,
+		String redirectUrl,
+		Long performanceId
+	) {
+		public static CarouselFindResponse from(Promotion promotion) {
+			return new CarouselFindResponse(
+				promotion.getId(),
+				promotion.getCarouselNumber(),
+				promotion.getPromotionPhoto(),
+				promotion.isExternal(),
+				promotion.getRedirectUrl(),
+				Optional.ofNullable(promotion.getPerformance())
+					.map(Performance::getId)
+					.orElse(null)
+			);
+		}
+	}
 }
--- src/main/java/com/beat/domain/admin/application/dto/UserFindAllResponse.java
+++ src/main/java/com/beat/domain/admin/application/dto/UserFindAllResponse.java
@@ -5,24 +5,24 @@ import com.beat.domain.user.domain.Users;
 import java.util.List;
 
 public record UserFindAllResponse(
-        List<UserFindResponse> users
+	List<UserFindResponse> users
 ) {
-    public static UserFindAllResponse from(List<Users> users) {
-        List<UserFindResponse> userFindResponses = users.stream()
-                .map(UserFindResponse::from)
-                .toList();
-        return new UserFindAllResponse(userFindResponses);
-    }
+	public static UserFindAllResponse from(List<Users> users) {
+		List<UserFindResponse> userFindResponses = users.stream()
+			.map(UserFindResponse::from)
+			.toList();
+		return new UserFindAllResponse(userFindResponses);
+	}
 
-    public record UserFindResponse(
-            Long id,
-            String role
-    ) {
-        public static UserFindResponse from(Users user) {
-            return new UserFindResponse(
-                    user.getId(),
-                    user.getRole().getRoleName()
-            );
-        }
-    }
+	public record UserFindResponse(
+		Long id,
+		String role
+	) {
+		public static UserFindResponse from(Users user) {
+			return new UserFindResponse(
+				user.getId(),
+				user.getRole().getRoleName()
+			);
+		}
+	}
 }
