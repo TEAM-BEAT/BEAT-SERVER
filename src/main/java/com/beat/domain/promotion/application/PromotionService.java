edfe9c1c96a81c540169b126632646ca7674f6d4
DongHoon Lee <dhl1924@naver.com>
2024-09-22 13:29:59 +0900

    [#209] feat(PromotionService): 프로모션 등록/수정/삭제/캐러셀 넘버 반환 메서드 구현

--- src/main/java/com/beat/domain/promotion/application/PromotionService.java
+++ src/main/java/com/beat/domain/promotion/application/PromotionService.java
@@ -1,9 +1,16 @@
 package com.beat.domain.promotion.application;
 
+import com.beat.domain.admin.application.dto.request.CarouselProcessRequest.PromotionModifyRequest;
+import com.beat.domain.performance.domain.Performance;
 import com.beat.domain.promotion.dao.PromotionRepository;
+import com.beat.domain.promotion.domain.CarouselNumber;
 import com.beat.domain.promotion.domain.Promotion;
+import com.beat.domain.promotion.exception.PromotionErrorCode;
 import com.beat.domain.promotion.port.in.PromotionUseCase;
+import com.beat.global.common.exception.NotFoundException;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,11 +20,57 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PromotionService implements PromotionUseCase {
 
-    private final PromotionRepository promotionRepository;
+	private final PromotionRepository promotionRepository;
 
-    @Override
-    @Transactional(readOnly = true)
-    public List<Promotion> findAllPromotions() {
-        return promotionRepository.findAll();
-    }
+	@Override
+	@Transactional(readOnly = true)
+	public Promotion findById(Long promotionId) {
+		return promotionRepository.findById(promotionId)
+			.orElseThrow(() -> new NotFoundException(PromotionErrorCode.PROMOTION_NOT_FOUND));
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Promotion> findAllPromotions() {
+		return promotionRepository.findAll();
+	}
+
+	@Override
+	@Transactional
+	public Promotion createPromotion(String newImageUrl, Performance performance, String redirectUrl,
+		boolean isExternal, CarouselNumber carouselNumber) {
+		Promotion newPromotion = Promotion.create(
+			newImageUrl,
+			performance,
+			redirectUrl,
+			isExternal,
+			carouselNumber
+		);
+		return promotionRepository.save(newPromotion);
+	}
+
+	@Override
+	@Transactional
+	public Promotion modifyPromotion(Promotion promotion, Performance performance, PromotionModifyRequest request) {
+		promotion.updatePromotionDetails(
+			request.carouselNumber(),
+			request.newImageUrl(),
+			request.isExternal(),
+			request.redirectUrl(),
+			performance
+		);
+		return promotionRepository.save(promotion);
+	}
+
+	@Override
+	@Transactional
+	public void deleteByCarouselNumber(CarouselNumber carouselNumber) {
+		promotionRepository.deleteByCarouselNumber(carouselNumber);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<CarouselNumber> findAllCarouselNumbers() {
+		return promotionRepository.findAllCarouselNumbers();
+	}
 }
