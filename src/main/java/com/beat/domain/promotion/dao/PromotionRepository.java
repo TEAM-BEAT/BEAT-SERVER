54bad831678ee8167878b113d24cd72523013590
DongHoon Lee <dhl1924@naver.com>
2024-09-22 14:16:15 +0900

    [#209] fix(PromotionRepository): JPQL로 Enum 필드만 반환하도록 수정

--- src/main/java/com/beat/domain/promotion/dao/PromotionRepository.java
+++ src/main/java/com/beat/domain/promotion/dao/PromotionRepository.java
@@ -3,12 +3,14 @@ package com.beat.domain.promotion.dao;
 import com.beat.domain.promotion.domain.CarouselNumber;
 import com.beat.domain.promotion.domain.Promotion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PromotionRepository extends JpaRepository<Promotion, Long> {
     List<Promotion> findAll();
 
+    @Query("SELECT p.carouselNumber FROM Promotion p")
     List<CarouselNumber> findAllCarouselNumbers();
 
     void deleteByCarouselNumber(CarouselNumber carouselNumber);
