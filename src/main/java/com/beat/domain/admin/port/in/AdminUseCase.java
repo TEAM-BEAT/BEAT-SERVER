9c16d1aa693e46a29756cd4a6338614217ca3116
DongHoon Lee <dhl1924@naver.com>
2024-09-22 13:26:39 +0900

    [#209] feat(AdminUseCase): 프로모션 등록/수정/삭제 메서드 선언

--- src/main/java/com/beat/domain/admin/port/in/AdminUseCase.java
+++ src/main/java/com/beat/domain/admin/port/in/AdminUseCase.java
@@ -1,9 +1,15 @@
 package com.beat.domain.admin.port.in;
 
+import com.beat.domain.admin.application.dto.request.CarouselProcessRequest.PromotionGenerateRequest;
+import com.beat.domain.admin.application.dto.request.CarouselProcessRequest.PromotionModifyRequest;
+import com.beat.domain.promotion.domain.CarouselNumber;
 import com.beat.domain.promotion.domain.Promotion;
 
 import java.util.List;
 
 public interface AdminUseCase {
-    List<Promotion> findAllPromotionsSortedByCarouselNumber();
+	List<Promotion> findAllPromotionsSortedByCarouselNumber();
+
+	List<Promotion> handlePromotions(List<PromotionModifyRequest> modifyRequests,
+		List<PromotionGenerateRequest> generateRequests, List<CarouselNumber> deleteCarouselNumbers);
 }
