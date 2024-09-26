70747290fdd56e214c3b2a37ed43521a9588cbc0
DongHoon Lee <125895298+hoonyworld@users.noreply.github.com>
2024-09-26 23:58:18 +0900

    [#212] fix(CurrentMember): 파라미터 옵션 hidden으로 설정 (#213)

--- src/main/java/com/beat/global/auth/annotation/CurrentMember.java
+++ src/main/java/com/beat/global/auth/annotation/CurrentMember.java
@@ -6,8 +6,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import io.swagger.v3.oas.annotations.Parameter;
+
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@Parameter(hidden = true)
 public @interface CurrentMember {
 }
