8f7c7469af46e9a1198f8d75223fcca0ad192eac
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-09-26 21:16:56 +0900

    [#211] fix(PerformanceErrorCode):PAST_SCHEDULE_NOT_ALLOWED 추가 및 코드 포맷팅 적용

--- src/main/java/com/beat/domain/performance/exception/PerformanceErrorCode.java
+++ src/main/java/com/beat/domain/performance/exception/PerformanceErrorCode.java
@@ -1,27 +1,28 @@
 package com.beat.domain.performance.exception;
 
 import com.beat.global.common.exception.base.BaseErrorCode;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum PerformanceErrorCode implements BaseErrorCode {
-    PERFORMANCE_NOT_FOUND(404, "해당 공연 정보를 찾을 수 없습니다."),
-    REQUIRED_DATA_MISSING(400, "필수 데이터가 누락되었습니다."),
-    INVALID_DATA_FORMAT(400, "잘못된 데이터 형식입니다."),
-    INVALID_REQUEST_FORMAT(400, "잘못된 요청 형식입니다."),
-    PRICE_UPDATE_NOT_ALLOWED(400, "예매자가 존재하여 가격을 수정할 수 없습니다."),
-    NEGATIVE_TICKET_PRICE(400, "티켓 가격은 음수일 수 없습니다."),
-    NO_PERFORMANCE_FOUND(404, "공연을 찾을 수 없습니다."),
-    PERFORMANCE_DELETE_FAILED(403, "예매자가 1명 이상 있을 경우, 공연을 삭제할 수 없습니다."),
-    NOT_PERFORMANCE_OWNER(403, "해당 공연의 메이커가 아닙니다."),
-    MAX_SCHEDULE_LIMIT_EXCEEDED(400, "공연 회차는 최대 10개까지 추가할 수 있습니다."),
-    INVALID_PERFORMANCE_DESCRIPTION_LENGTH(400, "공연 소개 글자수가 500자를 초과했습니다."),
-    INVALID_ATTENTION_NOTE_LENGTH(400, "공연 유의사항 글자수가 500자를 초과했습니다."),
-    INTERNAL_SERVER_ERROR(500, "서버 내부 오류입니다.")
-    ;
+	PERFORMANCE_NOT_FOUND(404, "해당 공연 정보를 찾을 수 없습니다."),
+	REQUIRED_DATA_MISSING(400, "필수 데이터가 누락되었습니다."),
+	INVALID_DATA_FORMAT(400, "잘못된 데이터 형식입니다."),
+	INVALID_REQUEST_FORMAT(400, "잘못된 요청 형식입니다."),
+	PRICE_UPDATE_NOT_ALLOWED(400, "예매자가 존재하여 가격을 수정할 수 없습니다."),
+	NEGATIVE_TICKET_PRICE(400, "티켓 가격은 음수일 수 없습니다."),
+	NO_PERFORMANCE_FOUND(404, "공연을 찾을 수 없습니다."),
+	PERFORMANCE_DELETE_FAILED(403, "예매자가 1명 이상 있을 경우, 공연을 삭제할 수 없습니다."),
+	NOT_PERFORMANCE_OWNER(403, "해당 공연의 메이커가 아닙니다."),
+	MAX_SCHEDULE_LIMIT_EXCEEDED(400, "공연 회차는 최대 10개까지 추가할 수 있습니다."),
+	INVALID_PERFORMANCE_DESCRIPTION_LENGTH(400, "공연 소개 글자수가 500자를 초과했습니다."),
+	INVALID_ATTENTION_NOTE_LENGTH(400, "공연 유의사항 글자수가 500자를 초과했습니다."),
+	INTERNAL_SERVER_ERROR(500, "서버 내부 오류입니다."),
+	PAST_SCHEDULE_NOT_ALLOWED(400, "과거 날짜 스케쥴을 포함한 공연을 생성할 수 없습니다.");
 
-    private final int status;
-    private final String message;
+	private final int status;
+	private final String message;
 }
