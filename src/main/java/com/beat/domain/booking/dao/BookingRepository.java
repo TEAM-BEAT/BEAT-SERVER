aa65eb80da3ca2d20f5c48924d2570f37e77b612
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-09-30 18:22:28 +0900

    [#211] fix(BookingRepository): 예매자 존재 여부 확인 로직 변경

--- src/main/java/com/beat/domain/booking/dao/BookingRepository.java
+++ src/main/java/com/beat/domain/booking/dao/BookingRepository.java
@@ -1,37 +1,38 @@
 package com.beat.domain.booking.dao;
 
-import com.beat.domain.booking.domain.Booking;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-import java.util.Optional;
+import com.beat.domain.booking.domain.Booking;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
-    @Query("SELECT b FROM Booking b " +
-            "JOIN b.schedule s " +
-            "JOIN s.performance p " +
-            "WHERE b.bookerName = :bookerName " +
-            "AND b.bookerPhoneNumber = :bookerPhoneNumber " +
-            "AND b.password = :password " +
-            "AND b.birthDate = :birthDate")
-    Optional<List<Booking>> findByBookerNameAndBookerPhoneNumberAndPasswordAndBirthDate(
-            @Param("bookerName") String bookerName,
-            @Param("bookerPhoneNumber") String bookerPhoneNumber,
-            @Param("password") String password,
-            @Param("birthDate") String birthDate
-    );
+	@Query("SELECT b FROM Booking b " +
+		"JOIN b.schedule s " +
+		"JOIN s.performance p " +
+		"WHERE b.bookerName = :bookerName " +
+		"AND b.bookerPhoneNumber = :bookerPhoneNumber " +
+		"AND b.password = :password " +
+		"AND b.birthDate = :birthDate")
+	Optional<List<Booking>> findByBookerNameAndBookerPhoneNumberAndPasswordAndBirthDate(
+		@Param("bookerName") String bookerName,
+		@Param("bookerPhoneNumber") String bookerPhoneNumber,
+		@Param("password") String password,
+		@Param("birthDate") String birthDate
+	);
 
-    Optional<Booking> findFirstByBookerNameAndBookerPhoneNumberAndBirthDateAndPassword(
-            String bookerName,
-            String bookerPhoneNumber,
-            String birthDate,
-            String password
-    );
+	Optional<Booking> findFirstByBookerNameAndBookerPhoneNumberAndBirthDateAndPassword(
+		String bookerName,
+		String bookerPhoneNumber,
+		String birthDate,
+		String password
+	);
 
-    List<Booking> findByUsersId(Long userId);
+	List<Booking> findByUsersId(Long userId);
 
-    @Query("SELECT COUNT(b) > 0 FROM Booking b WHERE b.schedule.id IN :scheduleIds")
-    boolean existsByScheduleIdIn(@Param("scheduleIds") List<Long> scheduleIds);
+	@Query("SELECT COUNT(b) > 0 FROM Booking b WHERE b.schedule.id IN :scheduleIds AND b.bookingStatus != 'BOOKING_CANCELLED'")
+	boolean existsByScheduleIdIn(@Param("scheduleIds") List<Long> scheduleIds);
 }
