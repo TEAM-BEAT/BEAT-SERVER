05392066fb91019eb61137e57088378b92d1f759
DongHoon Lee <dhl1924@naver.com>
2024-10-02 02:54:14 +0900

    [#221] refactor(HomePromotionDetail): 정적 팩토리 메서드 내에서 초기화해서 return 하도록 변경

--- src/main/java/com/beat/domain/performance/application/dto/home/HomePromotionDetail.java
+++ src/main/java/com/beat/domain/performance/application/dto/home/HomePromotionDetail.java
@@ -1,13 +1,19 @@
 package com.beat.domain.performance.application.dto.home;
 
-public record HomePromotionDetail(
-        Long promotionId,
-        String promotionPhoto,
-        Long performanceId,
-        String redirectUrl,
-        boolean isExternal
-) {
-    public static HomePromotionDetail of(Long promotionId, String promotionPhoto, Long performanceId, String redirectUrl, boolean isExternal) {
-        return new HomePromotionDetail(promotionId, promotionPhoto, performanceId, redirectUrl, isExternal);
-    }
+import com.beat.domain.promotion.domain.CarouselNumber;
+import com.beat.domain.promotion.domain.Promotion;
+
+public record HomePromotionDetail(Long promotionId, String promotionPhoto, Long performanceId, String redirectUrl,
+								  boolean isExternal, CarouselNumber carouselNumber) {
+
+	public static HomePromotionDetail from(Promotion promotion) {
+		Long performanceId = null;
+
+		if (promotion.getPerformance() != null) {
+			performanceId = promotion.getPerformance().getId();
+		}
+
+		return new HomePromotionDetail(promotion.getId(), promotion.getPromotionPhoto(), performanceId,
+			promotion.getRedirectUrl(), promotion.isExternal(), promotion.getCarouselNumber());
+	}
 }
