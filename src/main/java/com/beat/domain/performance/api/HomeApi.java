261853609392eae58b781e08e6691379a907e333
DongHoon Lee <dhl1924@naver.com>
2024-10-02 02:56:35 +0900

    [#221] feat(HomeApi): Home API 스웨거 명세서 작성

---
+++ src/main/java/com/beat/domain/performance/api/HomeApi.java
@@ -0,0 +1,33 @@
+package com.beat.domain.performance.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.beat.domain.performance.application.dto.home.HomeFindAllResponse;
+import com.beat.domain.performance.domain.Genre;
+import com.beat.global.common.dto.ErrorResponse;
+import com.beat.global.common.dto.SuccessResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Home", description = "홈 화면에서 공연 및 홍보목록 조회 API")
+public interface HomeApi {
+
+	@Operation(summary = "전체 공연 및 홍보 목록 조회", description = "홈 화면에서 전체 공연 목록 및 홍보 목록을 조회하는 GET API")
+	@ApiResponses(
+		value = {
+			@ApiResponse(
+				responseCode = "200",
+				description = "홈 화면 공연 목록 조회가 성공적으로 완료되었습니다.",
+				content = @Content(schema = @Schema(implementation = SuccessResponse.class))
+			)
+		}
+	)
+	ResponseEntity<SuccessResponse<HomeFindAllResponse>> getHomePerformanceList(
+		@RequestParam(required = false) Genre genre);
+}
