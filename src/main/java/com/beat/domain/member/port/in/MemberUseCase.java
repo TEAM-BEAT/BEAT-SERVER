6b28378de573d127df0660322af1536c57ec9afc
DongHoon Lee <125895298+hoonyworld@users.noreply.github.com>
2024-10-06 17:11:52 +0900

    [HOTFIX] #233 - memberId로 User객체를 잘못 조회했던 문제 해결 (#234)
    * [#233] refactor(MemberUseCase): 메서드 이름 변경 및 추가 메서드 선언
    
    * [#233] refactor(AdminFacade): 메서드 이름 변경사항 반영
    
    * [#233] refactor(AdminFacade): findUserByMemberId 삭제 및 메서드 이름 변경 및 읽기 전용 트랜잭션 선언
    
    * [#233] feat(UserUseCase): userId로 User 객체를 조회하는 메서드 선언
    
    * [#233] feat(UserService): userId로 User 객체를 조회하는 메서드 구현
    
    * [#233] fix(SocialLoginService): memberId로 Member 객체를 조회 후 그 회원의 userId로 User 객체를 조회하도록 수정
    
    * [#233] refactor(SocialLoginService): Member 객체에서 직접 Users 객체를 참조하도록 변경

--- src/main/java/com/beat/domain/member/port/in/MemberUseCase.java
+++ src/main/java/com/beat/domain/member/port/in/MemberUseCase.java
@@ -1,7 +1,14 @@
 package com.beat.domain.member.port.in;
 
 import com.beat.domain.member.domain.Member;
+import com.beat.domain.member.domain.SocialType;
 
 public interface MemberUseCase {
-    Member findMemberById(Long memberId);
+    Member findMemberByMemberId(Long memberId);
+
+    boolean checkMemberExistsBySocialIdAndSocialType(Long socialId, SocialType socialType);
+
+    Member findMemberBySocialIdAndSocialType(Long socialId, SocialType socialType);
+
+    void deleteUser(Long id);
 }
--- src/main/java/com/beat/domain/user/port/in/UserUseCase.java
+++ src/main/java/com/beat/domain/user/port/in/UserUseCase.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface UserUseCase {
     List<Users> findAllUsers();
+
+    Users findUserByUserId(final Long userId);
 }
