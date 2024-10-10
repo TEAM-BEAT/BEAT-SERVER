4d9143d2dbf2a1d1080695e02b917cf1601a5aa4
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-10-11 00:35:17 +0900

    [#241] refactor(PerformanceApi): performance swagger 문서화 수정

--- src/main/java/com/beat/domain/performance/api/PerformanceApi.java
+++ src/main/java/com/beat/domain/performance/api/PerformanceApi.java
@@ -13,9 +13,12 @@ import com.beat.domain.performance.application.dto.modify.PerformanceModifyReque
 import com.beat.domain.performance.application.dto.modify.PerformanceModifyResponse;
 import com.beat.domain.performance.application.dto.performanceDetail.PerformanceDetailResponse;
 import com.beat.global.auth.annotation.CurrentMember;
+import com.beat.global.common.dto.ErrorResponse;
 import com.beat.global.common.dto.SuccessResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -24,47 +27,169 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 public interface PerformanceApi {
 
 	@Operation(summary = "공연 생성 API", description = "공연을 생성하는 POST API입니다.")
+	@ApiResponses(
+		value = {
+			@ApiResponse(
+				responseCode = "201",
+				description = "공연이 성공적으로 생성되었습니다.",
+				content = @Content(schema = @Schema(implementation = SuccessResponse.class))
+			),
+			@ApiResponse(
+				responseCode = "400",
+				description = "필수 데이터가 누락되었습니다.",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+			),
+			@ApiResponse(
+				responseCode = "404",
+				description = "공연 정보를 찾을 수 없습니다.",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+			)
+		}
+	)
 	ResponseEntity<SuccessResponse<PerformanceResponse>> createPerformance(
 		@CurrentMember Long memberId,
-		@RequestBody PerformanceRequest performanceRequest);
+		@RequestBody PerformanceRequest performanceRequest
+	);
 
 	@Operation(summary = "공연 정보 수정 API", description = "공연 정보를 수정하는 PUT API입니다.")
-	@ApiResponses({
-		@ApiResponse(responseCode = "200", description = "공연 정보 수정 성공"),
-		@ApiResponse(responseCode = "400", description = "잘못된 요청 - 회차 최대 개수 초과"),
-		@ApiResponse(responseCode = "400", description = "잘못된 요청 - 티켓 가격은 음수일 수 없습니다."),
-		@ApiResponse(responseCode = "400", description = "잘못된 요청 - 예매자가 존재하여 가격을 수정할 수 없습니다."),
-		@ApiResponse(responseCode = "403", description = "권한 없음 - 해당 공연의 소유자가 아닙니다."),
-		@ApiResponse(responseCode = "404", description = "존재하지 않는 공연 ID로 수정 요청을 보낼 수 없습니다."),
-		@ApiResponse(responseCode = "404", description = "존재하지 않는 회원 ID로 수정 요청을 보낼 수 없습니다."),
-		@ApiResponse(responseCode = "404", description = "존재하지 않는 회차 ID로 수정 요청을 보낼 수 없습니다."),
-		@ApiResponse(responseCode = "404", description = "존재하지 않는 등장인물 ID로 수정 요청을 보낼 수 없습니다."),
-		@ApiResponse(responseCode = "404", description = "존재하지 않는 스태프 ID로 수정 요청을 보낼 수 없습니다."),
-		@ApiResponse(responseCode = "404", description = "존재하지 않는 상세이미지 ID로 수정 요청을 보낼 수 없습니다."),
-		@ApiResponse(responseCode = "500", description = "서버 내부 오류")})
+	@ApiResponses(
+		value = {
+			@ApiResponse(
+				responseCode = "200",
+				description = "공연 정보 수정 성공",
+				content = @Content(schema = @Schema(implementation = SuccessResponse.class))
+			),
+			@ApiResponse(
+				responseCode = "400",
+				description = "공연 회차는 최대 10개까지 추가할 수 있습니다.",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+			),
+			@ApiResponse(
+				responseCode = "400",
+				description = "티켓 가격은 음수일 수 없습니다.",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+			),
+			@ApiResponse(
+				responseCode = "400",
+				description = "예매자가 존재하여 가격을 수정할 수 없습니다.",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+			),
+			@ApiResponse(
+				responseCode = "403",
+				description = "해당 공연의 소유자가 아닙니다.",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+			),
+			@ApiResponse(
+				responseCode = "404",
+				description = "공연 정보를 찾을 수 없습니다.",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+			)
+		}
+	)
 	ResponseEntity<SuccessResponse<PerformanceModifyResponse>> updatePerformance(
 		@CurrentMember Long memberId,
-		@RequestBody PerformanceModifyRequest performanceModifyRequest);
+		@RequestBody PerformanceModifyRequest performanceModifyRequest
+	);
 
 	@Operation(summary = "공연 수정 페이지 정보 조회 API", description = "공연 정보를 조회하는 GET API입니다.")
+	@ApiResponses(
+		value = {
+			@ApiResponse(
+				responseCode = "200",
+				description = "공연 수정 페이지 정보 조회 성공",
+				content = @Content(schema = @Schema(implementation = SuccessResponse.class))
+			),
+			@ApiResponse(
+				responseCode = "404",
+				description = "공연 정보를 찾을 수 없습니다.",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+			)
+		}
+	)
 	ResponseEntity<SuccessResponse<PerformanceModifyDetailResponse>> getPerformanceForEdit(
 		@CurrentMember Long memberId,
-		@PathVariable Long performanceId);
+		@PathVariable Long performanceId
+	);
 
 	@Operation(summary = "공연 상세정보 조회 API", description = "공연 상세페이지의 공연 상세정보를 조회하는 GET API입니다.")
+	@ApiResponses(
+		value = {
+			@ApiResponse(
+				responseCode = "200",
+				description = "공연 상세정보 조회 성공",
+				content = @Content(schema = @Schema(implementation = SuccessResponse.class))
+			),
+			@ApiResponse(
+				responseCode = "404",
+				description = "공연 정보를 찾을 수 없습니다.",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+			)
+		}
+	)
 	ResponseEntity<SuccessResponse<PerformanceDetailResponse>> getPerformanceDetail(
-		@PathVariable Long performanceId);
+		@PathVariable Long performanceId
+	);
 
 	@Operation(summary = "예매하기 관련 공연 정보 조회 API", description = "예매하기 페이지에서 필요한 예매 관련 공연 정보를 조회하는 GET API입니다.")
+	@ApiResponses(
+		value = {
+			@ApiResponse(
+				responseCode = "200",
+				description = "예매하기 관련 공연 정보 조회 성공",
+				content = @Content(schema = @Schema(implementation = SuccessResponse.class))
+			),
+			@ApiResponse(
+				responseCode = "404",
+				description = "공연 정보를 찾을 수 없습니다.",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+			)
+		}
+	)
 	ResponseEntity<SuccessResponse<BookingPerformanceDetailResponse>> getBookingPerformanceDetail(
-		@PathVariable Long performanceId);
+		@PathVariable Long performanceId
+	);
 
 	@Operation(summary = "회원이 등록한 공연 목록 조회 API", description = "회원이 등록한 공연 목록을 조회하는 GET API입니다.")
+	@ApiResponses(
+		value = {
+			@ApiResponse(
+				responseCode = "200",
+				description = "회원이 등록한 공연 목록 조회 성공",
+				content = @Content(schema = @Schema(implementation = SuccessResponse.class))
+			),
+			@ApiResponse(
+				responseCode = "404",
+				description = "회원 정보를 찾을 수 없습니다.",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+			)
+		}
+	)
 	ResponseEntity<SuccessResponse<MakerPerformanceResponse>> getUserPerformances(
-		@CurrentMember Long memberId);
+		@CurrentMember Long memberId
+	);
 
 	@Operation(summary = "공연 삭제 API", description = "공연을 삭제하는 DELETE API입니다.")
+	@ApiResponses(
+		value = {
+			@ApiResponse(
+				responseCode = "200",
+				description = "공연 삭제 성공",
+				content = @Content(schema = @Schema(implementation = SuccessResponse.class))
+			),
+			@ApiResponse(
+				responseCode = "403",
+				description = "공연의 소유자가 아니거나 예매자가 있어 삭제할 수 없습니다.",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+			),
+			@ApiResponse(
+				responseCode = "404",
+				description = "공연 정보를 찾을 수 없습니다.",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+			)
+		}
+	)
 	ResponseEntity<SuccessResponse<Void>> deletePerformance(
 		@CurrentMember Long memberId,
-		@PathVariable Long performanceId);
+		@PathVariable Long performanceId
+	);
 }
