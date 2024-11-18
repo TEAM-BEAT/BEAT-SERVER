bafef36bac07088bca55e49db58b90e5640c2c8c
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-11-18 16:43:38 +0900

    [fix] #243 - 공연 생성 및 수정 시 performancePeriod, scheduleNumber 부여 로직 추가 (#244)
    * [#243] feat(PerformanceErrorCode): schedulelist 에러코드 추가 및 불필요한 코드 삭제, 순서 변경
    
    * [#243] feat(Performance): updatePerformancePeriod, assignScheduleNumbers 로직 추가
    
    * [#243] refactor(PerformanceManagementService): 공연 생성 시 updatePerformancePeriod, assignScheduleNumbers 하도록 수정
    
    * [#243] fix(PerformanceModifyService): 공연 회차 수정 시 updatePerformancePeriod, assignScheduleNumbers 하도록 수정
    
    * [#243] docs: success, errorcode 주석달아 정리
    
    * [#243] docs(PerformanceManagementService): 추후 수정 필요한 부분 주석 추가
    
    * [#243] refactor(Performance): 코드 개선

--- src/main/java/com/beat/domain/cast/exception/CastErrorCode.java
+++ src/main/java/com/beat/domain/cast/exception/CastErrorCode.java
@@ -1,16 +1,23 @@
 package com.beat.domain.cast.exception;
 
 import com.beat.global.common.exception.base.BaseErrorCode;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum CastErrorCode implements BaseErrorCode {
-    CAST_NOT_BELONG_TO_PERFORMANCE(403,"해당 등장인물은 해당 공연에 속해 있지 않습니다."),
-    CAST_NOT_FOUND(404, "등장인물이 존재하지 않습니다.")
-    ;
+	/*
+	403 Forbidden
+	*/
+	CAST_NOT_BELONG_TO_PERFORMANCE(403, "해당 등장인물은 해당 공연에 속해 있지 않습니다."),
 
-    private final int status;
-    private final String message;
+	/*
+	404 NotFound
+	*/
+	CAST_NOT_FOUND(404, "등장인물이 존재하지 않습니다.");
+
+	private final int status;
+	private final String message;
 }
