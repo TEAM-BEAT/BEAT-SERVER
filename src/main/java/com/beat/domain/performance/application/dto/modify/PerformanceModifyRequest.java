384e5f7873d36bb7af7d340a38608641e6c63938
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-11-28 01:52:49 +0900

    [#257] refactor: valid 적용

--- src/main/java/com/beat/domain/performance/application/dto/modify/PerformanceModifyRequest.java
+++ src/main/java/com/beat/domain/performance/application/dto/modify/PerformanceModifyRequest.java
@@ -9,12 +9,16 @@ import com.beat.domain.performance.application.dto.modify.staff.StaffModifyReque
 import com.beat.domain.performance.domain.BankName;
 import com.beat.domain.performance.domain.Genre;
 
+import jakarta.validation.constraints.Size;
+
 public record PerformanceModifyRequest(
 	Long performanceId,
 	String performanceTitle,
 	Genre genre,
 	int runningTime,
+	@Size(max = 1500, message = "공연 소개는 1500자를 초과할 수 없습니다.")
 	String performanceDescription,
+	@Size(max = 1500, message = "공연 유의사항은 1500자를 초과할 수 없습니다.")
 	String performanceAttentionNote,
 	BankName bankName,
 	String accountNumber,
