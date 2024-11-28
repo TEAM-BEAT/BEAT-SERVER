bd09e0f6e9d71f6c2e0c132d3855cc0689483e5c
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-11-28 18:52:57 +0900

    [#261] feat(TicketErrorCode): TicketErrorCode 코드 추가

--- src/main/java/com/beat/domain/booking/exception/TicketErrorCode.java
+++ src/main/java/com/beat/domain/booking/exception/TicketErrorCode.java
@@ -11,7 +11,9 @@ public enum TicketErrorCode implements BaseErrorCode {
 	/*
 	400 BadRequest
 	*/
-	PAYMENT_COMPLETED_TICKET_UPDATE_NOT_ALLOWED(400, "이미 결제가 완료된 티켓의 상태는 변경할 수 없습니다.");
+	PAYMENT_COMPLETED_TICKET_UPDATE_NOT_ALLOWED(400, "이미 결제가 완료된 티켓의 상태는 변경할 수 없습니다."),
+	SEARCH_WORD_TOO_SHORT(400, "검색어는 최소 2글자 이상이어야 합니다."),
+	DELETED_TICKET_RETRIEVE_NOT_ALLOWED(400, "삭제된 예매자를 조회할 수 없습니다.");;
 
 	private final int status;
 	private final String message;
