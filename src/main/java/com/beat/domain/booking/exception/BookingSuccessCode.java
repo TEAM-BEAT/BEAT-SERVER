c925ec0557f99c3278e6469b4f8a33e2746d3e1c
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-11-28 22:14:39 +0900

    [#261] refactor: BookingSuccessCode에서 TicketSuccessCode 분리

--- src/main/java/com/beat/domain/booking/exception/BookingSuccessCode.java
+++ src/main/java/com/beat/domain/booking/exception/BookingSuccessCode.java
@@ -15,11 +15,6 @@ public enum BookingSuccessCode implements BaseSuccessCode {
 	GUEST_BOOKING_RETRIEVE_SUCCESS(200, "비회원 예매 조회가 성공적으로 완료되었습니다."),
 	BOOKING_REFUND_SUCCESS(200, "예매자의 환불요청이 성공했습니다."),
 	BOOKING_CANCEL_SUCCESS(200, "예매자의 취소요청이 성공했습니다."),
-	TICKET_RETRIEVE_SUCCESS(200, "예매자 목록 조회가 성공적으로 완료되었습니다."),
-	TICKET_UPDATE_SUCCESS(200, "예매자 입금여부 수정이 성공적으로 완료되었습니다."),
-	TICKET_REFUND_SUCCESS(200, "예매 환불처리 요청이 성공했습니다."),
-	TICKET_DELETE_SUCCESS(200, "예매자 삭제 요청이 성공했습니다."),
-	TICKET_SEARCH_SUCCESS(200, "예매자 검색 결과 조회가 성공적으로 완료되었습니다."),
 
 	/*
 	201 Created
