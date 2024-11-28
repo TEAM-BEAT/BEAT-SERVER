20e686498a5e3f35d8dccd5181ebdeb38a726cf0
hyerinhwang-sailin <sallyh1111@ewhain.net>
2024-11-29 01:41:52 +0900

    [#261] refactor(TicketDetail): 빈문자열 대신 enum 값 활용

--- src/main/java/com/beat/domain/booking/application/dto/TicketDetail.java
+++ src/main/java/com/beat/domain/booking/application/dto/TicketDetail.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 import com.beat.domain.booking.domain.BookingStatus;
+import com.beat.domain.performance.domain.BankName;
 
 public record TicketDetail(
 	Long bookingId,
@@ -39,7 +40,7 @@ public record TicketDetail(
 			createdAt,
 			bookingStatus,
 			scheduleNumber,
-			Objects.requireNonNullElse(bankName, ""),
+			Objects.requireNonNullElse(bankName, BankName.NONE.getDisplayName()),
 			Objects.requireNonNullElse(accountNumber, ""),
 			Objects.requireNonNullElse(accountHolder, "")
 		);
