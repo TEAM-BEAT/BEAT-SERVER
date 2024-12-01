a9ceea65d582723252e9ee4899b94dee5506be58
DongHoon Lee <dhl1924@naver.com>
2024-12-01 19:56:07 +0900

    [#268] chore(Performance): 쓰지 않는 import문 삭제

--- src/main/java/com/beat/domain/performance/domain/Performance.java
+++ src/main/java/com/beat/domain/performance/domain/Performance.java
@@ -27,7 +27,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
