5fe4d69833c1f68055afc84693c19b97595d52b7
DongHoon Lee <dhl1924@naver.com>
2024-12-06 16:29:49 +0900

    [#286] refactor(JpaAuditingConfig): JpaAuditingConfig 롤백

--- src/main/java/com/beat/global/common/config/JpaAuditingConfig.java
+++ src/main/java/com/beat/global/common/config/JpaAuditingConfig.java
@@ -1,23 +1,9 @@
 package com.beat.global.common.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import com.querydsl.jpa.impl.JPAQueryFactory;
-
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaAuditingConfig {
-
-	@PersistenceContext
-	private EntityManager em;
-
-	@Bean
-	public JPAQueryFactory jpaQueryFactory() {
-		return new JPAQueryFactory(em);
-	}
 }
