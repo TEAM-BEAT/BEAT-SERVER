b83520b8df716cc237dfe27df8b943ee7498ac86
DongHoon Lee <dhl1924@naver.com>
2024-12-28 20:20:12 +0900

    [#295] feat(MemberLoginResponse): 로그인 성공 응답 DTO 생성

---
+++ src/main/java/com/beat/domain/member/dto/MemberLoginResponse.java
@@ -0,0 +1,15 @@
+package com.beat.domain.member.dto;
+
+public record MemberLoginResponse(
+	String accessToken,
+	String nickname,
+	String role
+) {
+	public static MemberLoginResponse of(
+		final String accessToken,
+		final String nickname,
+		final String role
+	) {
+		return new MemberLoginResponse(accessToken, nickname, role);
+	}
+}
