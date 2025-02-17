d3889a95c34ac1e416c5096b5c99e0195dc6a1f2
DongHoon Lee <dhl1924@naver.com>
2025-02-17 17:08:24 +0900

    [#310] feat(ServiceLoggingAspect): 서비스 계층의 메서드 실행 및 반환 로깅 기능 추가
    - 서비스 메서드 실행 전 로깅 (@Before)
    - 서비스 메서드 정상 반환 시 로깅 (@AfterReturning)
    - 서비스 메서드 실행 후 로깅 (@After)
    - 예외 발생 시 로깅 (@AfterThrowing)
    - 실행 시간 측정을 위한 @Around 로직 추가
    - Order(3) 적용하여 트랜잭션 로깅 및 컨트롤러 로깅 이후에 실행되도록 설정

---
+++ src/main/java/com/beat/global/common/aop/ServiceLoggingAspect.java
@@ -0,0 +1,73 @@
+package com.beat.global.common.aop;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Aspect
+@Order(3)
+@Component
+public class ServiceLoggingAspect {
+	/** 실행 시간 측정 */
+	@Around("com.beat.global.common.aop.Pointcuts.allApplicationLogic()")
+	public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+		long start = System.currentTimeMillis();
+		try {
+			return joinPoint.proceed();
+		} finally {
+			long end = System.currentTimeMillis();
+			long timeInMs = end - start;
+			log.info("[실행 시간] {}.{}() | time = {}ms",
+				joinPoint.getSignature().getDeclaringType().getSimpleName(),
+				joinPoint.getSignature().getName(),
+				timeInMs);
+		}
+	}
+
+	/** Service 메서드 실행 전 로깅 */
+	@Before("com.beat.global.common.aop.Pointcuts.allService()")
+	public void doLog(JoinPoint joinPoint) {
+		log.info("[메서드 실행] {}.{}() | 인자: {}",
+			joinPoint.getSignature().getDeclaringType().getSimpleName(),
+			joinPoint.getSignature().getName(),
+			Arrays.toString(joinPoint.getArgs()));
+	}
+
+	/** Service 정상 반환 로깅 */
+	@AfterReturning(value = "com.beat.global.common.aop.Pointcuts.allService()", returning = "result")
+	public void logReturn(JoinPoint joinPoint, Object result) {
+		log.info("[Service 정상 반환] {}.{}() | 반환 값: {}",
+			joinPoint.getSignature().getDeclaringType().getSimpleName(),
+			joinPoint.getSignature().getName(),
+			result);
+	}
+
+	/** 예외 발생 시 로깅 */
+	@AfterThrowing(value = "com.beat.global.common.aop.Pointcuts.allService()", throwing = "ex")
+	public void logException(JoinPoint joinPoint, Exception ex) {
+		log.error("[예외 발생] {}.{}() | 예외 메시지: {}",
+			joinPoint.getSignature().getDeclaringType().getSimpleName(),
+			joinPoint.getSignature().getName(),
+			ex.getMessage());
+	}
+
+	/** 메서드 실행 후 로깅 */
+	@After("com.beat.global.common.aop.Pointcuts.allService()")
+	public void doAfter(JoinPoint joinPoint) {
+		log.info("[메서드 종료] {}.{}()",
+			joinPoint.getSignature().getDeclaringType().getSimpleName(),
+			joinPoint.getSignature().getName());
+	}
+}
