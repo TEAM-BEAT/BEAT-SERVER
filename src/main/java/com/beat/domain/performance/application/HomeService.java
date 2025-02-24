8a940e087f5921b0516b879543d2aef380043581
DongHoon Lee <dhl1924@naver.com>
2025-02-25 00:08:12 +0900

    [#308] refactor(HomeService): 홈 조회 메서드 리팩토링

--- src/main/java/com/beat/domain/performance/application/HomeService.java
+++ src/main/java/com/beat/domain/performance/application/HomeService.java
@@ -1,8 +1,12 @@
 package com.beat.domain.performance.application;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +21,7 @@ import com.beat.domain.performance.domain.Performance;
 import com.beat.domain.promotion.domain.Promotion;
 import com.beat.domain.promotion.port.in.PromotionUseCase;
 import com.beat.domain.schedule.application.ScheduleService;
+import com.beat.domain.schedule.application.dto.response.MinPerformanceDateResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -33,22 +38,33 @@ public class HomeService {
 	public HomeFindAllResponse findHomePerformanceList(HomeFindRequest homeFindRequest) {
 
 		List<Performance> performances = findPerformancesByGenre(homeFindRequest);
-		List<HomePromotionDetail> promotions = findAllPromotionsSortedByCarouselNumber();
+		List<HomePromotionDetail> promotionDetails = findAllPromotionsSortedByCarouselNumber();
 
 		if (performances.isEmpty()) {
-			return HomeFindAllResponse.of(promotions, new ArrayList<>());
+			return HomeFindAllResponse.of(promotionDetails, new ArrayList<>());
 		}
 
-		List<HomePerformanceDetail> sortedPerformances = performances.stream()
-			.map(performance -> {
-				int minDueDate = scheduleService.getMinDueDateForPerformance(performance.getId());
-				return HomePerformanceDetail.of(performance, minDueDate);
-			})
-			.sorted(Comparator.<HomePerformanceDetail>comparingInt(detail -> detail.dueDate() < 0 ? 1 : 0)
-				.thenComparingInt(detail -> Math.abs(detail.dueDate())))
+		List<Long> performanceIds = performances.stream()
+			.map(Performance::getId)
 			.toList();
 
-		return HomeFindAllResponse.of(promotions, sortedPerformances);
+		MinPerformanceDateResponse minPerformanceDateResponse = scheduleService.retrieveMinPerformanceDateByPerformanceIds(
+			performanceIds);
+		Map<Long, LocalDateTime> minPerformanceDateMap = minPerformanceDateResponse.performanceDateMap();
+
+		List<HomePerformanceDetail> sortedPerformances = performances.stream()
+			.map(performance -> HomePerformanceDetail.of(performance,
+				calculateDueDate(minPerformanceDateMap.get(performance.getId()))))
+			.sorted(Comparator.comparingInt((HomePerformanceDetail detail) -> {
+				if (detail.dueDate() < 0) {
+					return 1;
+				}
+
+				return 0;
+			}).thenComparingInt(detail -> Math.abs(detail.dueDate())))
+			.toList();
+
+		return HomeFindAllResponse.of(promotionDetails, sortedPerformances);
 	}
 
 	private List<Performance> findPerformancesByGenre(HomeFindRequest homeFindRequest) {
@@ -67,5 +83,12 @@ public class HomeService {
 			.sorted(Comparator.comparing(Promotion::getCarouselNumber, Comparator.comparingInt(Enum::ordinal)))
 			.map(HomePromotionDetail::from)
 			.toList();
+	}
+
+	private int calculateDueDate(LocalDateTime baseDateTime) {
+		if (baseDateTime == null) {
+			return Integer.MAX_VALUE;
+		}
+		return (int)ChronoUnit.DAYS.between(LocalDate.now(), baseDateTime.toLocalDate());
 	}
 }
