45169e42f17f4cfc2e62a948573d5131176da352
DongHoon Lee <dhl1924@naver.com>
2025-03-07 18:15:46 +0900

    [#308] chore(ScheduleRepositoryCustomImpl): 안쓰는 import문 정리

--- src/main/java/com/beat/domain/schedule/dao/ScheduleRepositoryCustomImpl.java
+++ src/main/java/com/beat/domain/schedule/dao/ScheduleRepositoryCustomImpl.java
@@ -1,8 +1,6 @@
 package com.beat.domain.schedule.dao;
 
-
 import static com.beat.domain.schedule.domain.QSchedule.*;
-import static org.hibernate.query.results.Builders.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,10 +9,6 @@ import org.springframework.stereotype.Repository;
 
 import com.beat.domain.schedule.dao.dto.MinPerformanceDateDto;
 import com.beat.domain.schedule.dao.dto.QMinPerformanceDateDto;
-import com.beat.domain.schedule.domain.QSchedule;
-import com.querydsl.core.types.dsl.DateTimeExpression;
-import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
