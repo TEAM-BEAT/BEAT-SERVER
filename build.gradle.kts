42d07bd7f0a8d0041b2e03c529041a0ca0a80512
hyerinhwang-sailin <sallyh1111@ewhain.net>
2026-03-10 13:02:15 +0900

    build: 미사용 의존성 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,6 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework:spring-webflux")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
