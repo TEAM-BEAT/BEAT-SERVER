e1f7e9b49e504f1209660e391c9f3ddce977a2bf
DongHoon Lee <dhl1924@naver.com>
2026-08-04 03:53:47 +0900

    fix: 티켓 서비스 테스트 컴파일 오류 수정

--- apis/src/test/java/com/beat/apis/ticket/application/TicketServiceTest.java
+++ apis/src/test/java/com/beat/apis/ticket/application/TicketServiceTest.java
@@ -2,6 +2,7 @@ package com.beat.apis.ticket.application;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.never;
