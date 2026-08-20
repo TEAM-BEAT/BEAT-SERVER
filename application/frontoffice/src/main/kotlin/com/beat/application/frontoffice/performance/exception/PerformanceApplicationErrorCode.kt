468b2267ef0fb3df477bce8f4187a853deccffa9
DongHoon Lee <dhl1924@naver.com>
2026-08-21 00:43:28 +0900

    fix: Performance 애플리케이션 오류 코드 충돌 제거

--- application/frontoffice/src/main/kotlin/com/beat/application/frontoffice/performance/exception/PerformanceApplicationErrorCode.kt
+++ application/frontoffice/src/main/kotlin/com/beat/application/frontoffice/performance/exception/PerformanceApplicationErrorCode.kt
@@ -14,7 +14,7 @@ enum class PerformanceApplicationErrorCode(
     PERFORMANCE_DELETE_FAILED("PERFORMANCE_DELETE_NOT_ALLOWED", FrontofficeApplicationErrorType.FORBIDDEN, "예매자가 1명 이상 있을 경우, 공연을 삭제할 수 없습니다."),
     NOT_PERFORMANCE_OWNER("PERFORMANCE_FORBIDDEN", FrontofficeApplicationErrorType.FORBIDDEN, "해당 공연의 메이커가 아닙니다."),
     PERFORMANCE_NOT_FOUND("PERFORMANCE_NOT_FOUND", FrontofficeApplicationErrorType.NOT_FOUND, "해당 공연 정보를 찾을 수 없습니다."),
-    MEMBER_NOT_FOUND("MEMBER_NOT_FOUND", FrontofficeApplicationErrorType.NOT_FOUND, "회원이 없습니다"),
+    MEMBER_NOT_FOUND("PERFORMANCE_MEMBER_NOT_FOUND", FrontofficeApplicationErrorType.NOT_FOUND, "회원이 없습니다"),
     SCHEDULE_LIST_NOT_FOUND("PERFORMANCE_SCHEDULE_LIST_REQUIRED", FrontofficeApplicationErrorType.INVALID_INPUT, "스케쥴 리스트에 스케쥴이 없습니다."),
     DUPLICATE_MODIFICATION_ID("PERFORMANCE_DUPLICATE_MODIFICATION_ID", FrontofficeApplicationErrorType.INVALID_INPUT, "동일한 수정 대상이 중복되었습니다."),
     INVALID_MODIFICATION_REQUEST("PERFORMANCE_INVALID_MODIFICATION_REQUEST", FrontofficeApplicationErrorType.INVALID_INPUT, "공연 수정 요청이 올바르지 않습니다."),
